1d2d971c6dd3b98dfc126a5260e38aa221ad0d5e
Davvid Ochoa <dogcaesar@gmail.com>
2022-06-23 17:15:32 +0100

    unnecessary code removed

--- src/run_coloc.py
+++ src/run_coloc.py
@@ -57,52 +57,6 @@ def main(cfg: DictConfig) -> None:
     # Write output
     (colocWithGenes.write.mode("overwrite").parquet(cfg.coloc.output))
 
-    # TODO: New feature: compute model averaged effect size ratios
-    # https://github.com/tobyjohnson/gtx/blob/9afa9597a51d0ff44536bc5c8eddd901ab3e867c/R/coloc.R#L91
-
-    # For debugging
-    # (
-    #     coloc.filter(
-    #         (F.col("left_studyKey") == "gwas_NEALE2_20003_1140909872")
-    #         & (
-    #             F.col("right_studyKey")
-    #             == "sqtl_GTEx-sQTL_chr22:17791301:17806239:clu_21824:ENSG00000243156_Ovary"
-    #         )
-    #         & (F.col("left_lead_variant_id") == "22:16590692:CAA:C")
-    #         & (F.col("right_lead_variant_id") == "22:17806438:G:A")
-    #     ).show(vertical=True)
-    # )
-
-
-# Expected result
-# # -RECORD 0-------------------------------------
-# #  left_key              | NEALE2_20003_1140...
-# #  left_lead_variant_id  | 22:16590692:CAA:C
-# #  left_type             | gwas
-# #  left_key              | NEALE2_20003_1140...
-# #  right_key             | GTEx-sQTL_chr22:1...
-# #  right_lead_variant_id | 22:17806438:G:A
-# #  right_type            | sqtl
-# #  right_key             | GTEx-sQTL_chr22:1...
-# #  coloc_n_vars          | 64
-# #  left_logsum           | 7.661225695815798
-# #  right_logsum          | 9.419701640007158
-# #  logsum_left_right     | 10.026044478423552
-# #  priorc1               | 1.0E-4
-# #  priorc2               | 1.0E-4
-# #  priorc12              | 1.0E-5
-# #  lH0abf                | 0
-# #  lH1abf                | -1.549114676160384
-# #  lH2abf                | 0.20936126803097643
-# #  lH3abf                | -1.3406169647338686
-# #  lH4abf                | -1.4868809865466766
-# #  coloc_h0              | 0.3409377045755464
-# #  coloc_h1              | 0.07242743035215946
-# #  coloc_h2              | 0.4203387963569506
-# #  coloc_h3              | 0.0892179998555815
-# #  coloc_h4              | 0.07707806885976214
-# #  coloc_h4_h3           | 0.8639295768177896
-# #  coloc_log2_h4_h3      | -0.21101437892981328
 
 if __name__ == "__main__":
     # pylint: disable = no-value-for-parameter
