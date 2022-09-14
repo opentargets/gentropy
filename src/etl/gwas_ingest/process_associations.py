dd0d2e94ff2e002bea4da8626129169207c6242f
Daniel Suveges <daniel.suveges@protonmail.com>
2022-09-14 11:22:30 +0100

    feat: adding functions to effect harmonizataion

--- src/etl/gwas_ingest/process_associations.py
+++ src/etl/gwas_ingest/process_associations.py
@@ -410,6 +410,54 @@ def map_variants(
     return mapped_associations
 
 
+def get_reverse_complement(df: DataFrame, allele_col: str) -> DataFrame:
+    """
+    This function return with column of reverse complement allele of a spcified allele column
+
+    Args:
+      df (DataFrame): DataFrame
+      allele_col (str): the name of the column containing the allele
+
+    Returns:
+      A dataframe with a new column called revcomp_{allele_col}
+    """
+
+    return df.withColumn(
+        f"revcomp_{allele_col}",
+        f.when(
+            f.col(allele_col).rlike("[ACTG]+"),
+            f.reverse(f.translate(f.col(allele_col), "ACTG", "TGAC")),
+        ),
+    )
+
+
+def is_palindrom(df: DataFrame, allele_col: str) -> DataFrame:
+    """
+    It takes a dataframe and a column name, and returns a dataframe with a new column indicating
+    whether allele is palindrom or not
+
+    Args:
+      df (DataFrame): DataFrame
+      allele_col (str): str = 'allele'
+
+    Returns:
+      A dataframe with a new column called is_allele_palindrom.
+    """
+
+    return (
+        df
+        # Get reverse complement:
+        .transform(lambda df: get_reverse_complement(df, "allele"))
+        # Adding flag:
+        .withColumn(
+            f"is_{allele_col}_palindrom",
+            f.when(f.col(f"revcomp_{allele_col}") == f.col(allele_col), True).otherwise(
+                False
+            ),
+        ).drop(f"revcomp_{allele_col}")
+    )
+
+
 def ingest_gwas_catalog_associations(
     etl: ETLSession,
     gwas_association_path: str,
---
+++ tests/test_effect_harmonization.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pyspark.sql.functions as f
+import pytest
+
+from etl.gwas_ingest.effect_harmonization import get_reverse_complement, is_palindrom
+
+if TYPE_CHECKING:
+    from pyspark.sql import DataFrame, SparkSession
+
+
+@pytest.fixture
+def mock_allele_columns(spark: SparkSession) -> DataFrame:
+    return spark.createDataFrame(
+        [
+            {"allele": "A", "reverseComp": "T", "isPalindrom": False},
+            {"allele": "C", "reverseComp": "G", "isPalindrom": False},
+            {"allele": "T", "reverseComp": "A", "isPalindrom": False},
+            {"allele": "G", "reverseComp": "C", "isPalindrom": False},
+            {"allele": "AT", "reverseComp": "AT", "isPalindrom": True},
+            {"allele": "TTGA", "reverseComp": "TCAA", "isPalindrom": False},
+            {"allele": "-", "reverseComp": None, "isPalindrom": False},
+            {"allele": None, "reverseComp": None, "isPalindrom": False},
+            {"allele": "CATATG", "reverseComp": "CATATG", "isPalindrom": True},
+        ]
+    ).persist()
+
+
+@pytest.fixture
+def call_is_palindrom(mock_allele_columns: DataFrame) -> DataFrame:
+    return mock_allele_columns.transform(lambda df: is_palindrom(df, "allele"))
+
+
+@pytest.fixture
+def call_get_reverse_complement(mock_allele_columns: DataFrame) -> DataFrame:
+    return mock_allele_columns.transform(
+        lambda df: get_reverse_complement(df, "allele")
+    )
+
+
+def test_reverse_complement(call_get_reverse_complement: DataFrame) -> None:
+
+    assert (
+        call_get_reverse_complement.filter(
+            f.col("reverseComp") != f.col("revcomp_allele")
+        ).count()
+    ) == 0
+
+
+def test_is_palindrom(call_is_palindrom: DataFrame) -> None:
+
+    assert (
+        call_is_palindrom.filter(
+            f.col("isPalindrom") != f.col("is_allele_palindrom")
+        ).count()
+    ) == 0
