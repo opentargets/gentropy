d82f270e6d7a8531ccdc3d7d372af23cf51667ec
Daniel Suveges <daniel.suveges@protonmail.com>
2022-09-16 23:29:41 +0100

    chore: adding function to calculate z-score

--- src/etl/gwas_ingest/effect_harmonization.py
+++ src/etl/gwas_ingest/effect_harmonization.py
@@ -1,11 +1,49 @@
 from __future__ import annotations
 
+import sys
 from typing import TYPE_CHECKING
 
+import scipy.stats as st
 from pyspark.sql import functions as f
+from pyspark.sql import types as t
 
 if TYPE_CHECKING:
     from pyspark.sql import DataFrame
+
+
+def pval_to_zscore(df: DataFrame, pvalcol: str) -> DataFrame:
+    """
+    This function converts a p-value to a z-score
+
+    Args:
+      df (DataFrame): DataFrame
+      pvalcol (str): The name of the column containing the p-values.
+
+    Returns:
+      A dataframe with a 'zscore' column.
+    """
+    return (
+        df
+        # Converting string p-values to float:
+        .withColumn(
+            f"parsed_${pvalcol}", f.col(pvalcol).cast(t.FloatType())
+        ).withColumn(
+            f"parsed_${pvalcol}",
+            f.when(f.col(f"parsed_${pvalcol}") == 0, sys.float_info.min).otherwise(
+                f.col(f"parsed_${pvalcol}")
+            ),
+        )
+        # Convert pvalues to z-score:
+        .withColumn(
+            "zscore",
+            f.udf(
+                lambda pv: float(abs(st.norm.ppf(pv / 2))) if pv else None,
+                t.FloatType(),
+            )(f.col(f"parsed_${pvalcol}")),
+        )
+        # Dropping helper column:
+        .drop(f"parsed_${pvalcol}")
+    )
 
 
 def get_reverse_complement(df: DataFrame, allele_col: str) -> DataFrame:
