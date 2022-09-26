305415e0b6e57ce921caf20d436cf7d1714af4e6
David Ochoa <dogcaesar@gmail.com>
2022-09-26 14:57:26 +0100

    fix: flake FS002 issue resolved

--- src/etl/json/__init__.py
+++ src/etl/json/__init__.py
@@ -13,15 +13,11 @@ if TYPE_CHECKING:
 SCHEMA_DIR = os.path.join(os.path.dirname(__file__), "schemas")
 
 
-def validate_df_schema(df: DataFrame, schema_json: str):
+def validate_df_schema(df: DataFrame, schema_json: str) -> None:
     core_schema = json.loads(Path(SCHEMA_DIR, schema_json).read_text(encoding="utf-8"))
     expected_schema = StructType.fromJson(core_schema)
     observed_schema = df.schema
     missing_struct_fields = [x for x in observed_schema if x not in expected_schema]
-    error_message = "The {missing_struct_fields} StructFields are not included in the {schema_json} DataFrame schema: {all_struct_fields}".format(
-        missing_struct_fields=missing_struct_fields,
-        schema_json=schema_json,
-        all_struct_fields=expected_schema,
-    )
+    error_message = f"The {missing_struct_fields} StructFields are not included in the {schema_json} DataFrame schema: {expected_schema}"
     if missing_struct_fields:
         raise Exception(error_message)
