01362c330786752e745e119a4e7a4e6e69c02bd0
David Ochoa <dogcaesar@gmail.com>
2022-09-27 17:19:23 +0100

    feat: non-nullable fields required in validated df

--- src/etl/json/__init__.py
+++ src/etl/json/__init__.py
@@ -18,7 +18,15 @@ def validate_df_schema(df: DataFrame, schema_json: str) -> None:
     )
     expected_schema = StructType.fromJson(core_schema)
     observed_schema = df.schema
+    # Observed fields not in schema
     missing_struct_fields = [x for x in observed_schema if x not in expected_schema]
     error_message = f"The {missing_struct_fields} StructFields are not included in the {schema_json} DataFrame schema: {expected_schema}"
     if missing_struct_fields:
+        raise Exception(error_message)
+
+    # Mandatory fields not in dataset
+    mandatory_fields = [x for x in expected_schema if not x.nullable]
+    missing_required_fields = [x for x in mandatory_fields if x not in observed_schema]
+    error_message = f"The {missing_required_fields} StructFields are required but missing from the DataFrame schema: {expected_schema}"
+    if missing_required_fields:
         raise Exception(error_message)
