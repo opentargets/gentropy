89869d27ea2520358373a671acf4871cf0d5d023
Irene López <irene.lopezs@protonmail.com>
2022-10-07 13:53:47 +0100

    feat: update `variant_annotation.json` to new schema

--- src/run_variant_annotation.py
+++ src/run_variant_annotation.py
@@ -12,6 +12,7 @@ import hail as hl
 import pyspark.sql.functions as f
 
 from etl.common.ETLSession import ETLSession
+from etl.json import validate_df_schema
 
 # Population of interest:
 POPULATIONS = {
@@ -43,13 +44,13 @@ def main(cfg: DictConfig) -> None:
     ht = hl.read_table(
         cfg.etl.variant_annotation.inputs.gnomad_file,
         _load_refs=False,
-    )
+    ).head(1000)
 
     # Generate struct for alt. allele frequency in selected populations:
     population_indices = ht.globals.freq_index_dict.collect()[0]
     population_indices = {pop: population_indices[f"{pop}-adj"] for pop in POPULATIONS}
     ht = ht.annotate(
-        alleleFrequencies=hl.struct(
+        alleleFrequenciesRaw=hl.struct(
             **{pop: ht.freq[index].AF for pop, index in population_indices.items()}
         )
     )
@@ -61,70 +62,81 @@ def main(cfg: DictConfig) -> None:
     ht = ht.annotate(locus_GRCh37=hl.liftover(ht.locus, "GRCh37"))
 
     # Adding build-specific coordinates to the table:
-    ht = ht.annotate(
-        chromosome=ht.locus.contig.replace("chr", ""),
-        position=ht.locus.position,
-        chromosomeB37=ht.locus_GRCh37.contig.replace("chr", ""),
-        positionB37=ht.locus_GRCh37.position,
-        referenceAllele=ht.alleles[0],
-        alternateAllele=ht.alleles[1],
-        alleleType=ht.allele_info.allele_type,
-        cadd=ht.cadd.rename({"raw_score": "raw"}).drop("has_duplicate"),
-        vep=ht.vep.drop(
-            "assembly_name",
-            "allele_string",
-            "ancestral",
-            "context",
-            "end",
-            "id",
-            "input",
-            "intergenic_consequences",
-            "seq_region_name",
-            "start",
-            "strand",
-            "variant_class",
-        ).rename({"rsid": "rsIds"}),
+    ht = (
+        ht.annotate(
+            chromosome=ht.locus.contig.replace("chr", ""),
+            position=ht.locus.position,
+            chromosomeB37=ht.locus_GRCh37.contig.replace("chr", ""),
+            positionB37=ht.locus_GRCh37.position,
+            referenceAllele=ht.alleles[0],
+            alternateAllele=ht.alleles[1],
+            alleleType=ht.allele_info.allele_type,
+            cadd=ht.cadd.rename({"raw_score": "raw"}).drop("has_duplicate"),
+            vepRaw=ht.vep.drop(
+                "assembly_name",
+                "allele_string",
+                "ancestral",
+                "context",
+                "end",
+                "id",
+                "input",
+                "intergenic_consequences",
+                "seq_region_name",
+                "start",
+                "strand",
+                "variant_class",
+            ),
+        )
+        .rename({"rsid": "rsIds"})
+        .drop("vep")
     )
 
     # Convert data:
     variants = (
-        ht.select_globals()
-        .to_spark(flatten=False)
+        ht.select_globals().to_spark(flatten=False)
         # Drop non biallelic variants
         .filter(f.size(f.col("alleles")) == 2)
         # Creating new column based on the transcript_consequences
         .select(
-            "*",
-            f.expr(
-                "filter(vep.transcript_consequences, array -> array.canonical == True)"
-            ).alias("transcript_consequences"),
-            f.struct(
-                f.col("vep.most_severe_consequence").alias("mostSevereConsequence"),
-                f.col("vep.motif_feature_consequences").alias(
-                    "motifFeatureConsequences"
-                ),
-                f.col("vep.regulatory_feature_consequences").alias(
-                    "regulatoryFeatureConsequences"
-                ),
-                f.col("transcript_consequences").alias("transcriptConsequences"),
-            ).alias("vep"),
             f.concat_ws(
                 "_", "chromosome", "position", "referenceAllele", "alternateAllele"
             ).alias("id"),
+            "chromosome",
+            "position",
+            "referenceAllele",
+            "alternateAllele",
+            "chromosomeB37",
+            "positionB37",
+            "alleleType",
+            "rsIds",
             f.array(
                 *[
                     f.struct(
-                        f.col(f"alleleFrequencies.{pop}").alias("alleleFrequency"),
+                        f.col(f"alleleFrequenciesRaw.{pop}").alias("alleleFrequency"),
                         f.lit(pop).alias("populationName"),
                     )
                     for pop in POPULATIONS
                 ]
             ).alias("alleleFrequencies"),
+            "cadd",
+            f.struct(
+                f.col("vepRaw.most_severe_consequence").alias("mostSevereConsequence"),
+                f.col("vepRaw.motif_feature_consequences").alias(
+                    "motifFeatureConsequences"
+                ),
+                f.col("vepRaw.regulatory_feature_consequences").alias(
+                    "regulatoryFeatureConsequences"
+                ),
+                # Non canonical transcripts are filtered out
+                f.expr(
+                    "filter(vepRaw.transcript_consequences, array -> array.canonical == True)"
+                ).alias("transcriptConsequences"),
+            ).alias("vep"),
+            "filters",
         )
-        .drop("locus", "alleles", "transcript_consequences", "alleleFrequencies")
     )
 
-    # validate_df_schema(variants, "variant_annotation.json")
+    validate_df_schema(variants, "variant_annotation.json")
 
     # Writing data partitioned by chromosome:
     (
