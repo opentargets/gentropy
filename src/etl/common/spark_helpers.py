b09666e0c8169b1beea9d62528585e13d191e9e7
Irene López <irene.lopezs@protonmail.com>
2022-10-25 15:43:15 +0100

    refactor: addressing comments

--- src/etl/common/spark_helpers.py
+++ src/etl/common/spark_helpers.py
@@ -7,7 +7,7 @@ import pyspark.sql.functions as f
 from pyspark.sql import Window
 
 if TYPE_CHECKING:
-    from pyspark.sql import Column, DataFrame
+    from pyspark.sql import Column, DataFrame, WindowSpec
 
 
 def nullify_empty_array(column: Column) -> Column:
@@ -33,63 +33,82 @@ def nullify_empty_array(column: Column) -> Column:
     return f.when(f.size(column) != 0, column)
 
 
-def get_record_with_minimum_value(
-    df: DataFrame, grouping_col: str, sorting_col: str
-) -> DataFrame:
-    """Returns the record with the minimum value of the sorting column within each group of the grouping column."""
-    w = Window.partitionBy(grouping_col).orderBy(sorting_col)
+def get_top_ranked_in_window(df: DataFrame, w: WindowSpec) -> DataFrame:
+    """Returns the record with the top rank within each group of the window."""
     return (
         df.withColumn("row_number", f.row_number().over(w))
         .filter(f.col("row_number") == 1)
         .drop("row_number")
     )
+
+
+def get_record_with_minimum_value(
+    df: DataFrame, grouping_col: str, sorting_col: str
+) -> DataFrame:
+    """Returns the record with the minimum value of the sorting column within each group of the grouping column.
+
+    Args:
+        df (DataFrame): The DataFrame to be processed.
+        grouping_col (str): The column name(s) to group the DataFrame by.
+        sorting_col (str): The column name to sort the DataFrame by.
+
+    Returns:
+        DataFrame: The DataFrame with the record with the minimum value of the sorting column within each group of the grouping column.
+
+    Examples:
+    >>> df = spark.createDataFrame(
+    ...     [[16, 5738837, "snv", "16_5738837_G_C"],
+    ...     [16, 5738892, "snv", "16_5738892_A_G"],
+    ...     [17, 5738922, "del", "16_5738922_CA_C"]],
+    ...     ["chromosome", "position", "alleleType", "id"])
+    >>> df.transform(
+    ...     lambda df: get_record_with_minimum_value(
+    ...         df, "chromosome", "position"
+    ...     )
+    ... ).show()
+    +----------+--------+----------+---------------+
+    |chromosome|position|alleleType|             id|
+    +----------+--------+----------+---------------+
+    |        17| 5738922|       del|16_5738922_CA_C|
+    |        16| 5738837|       snv| 16_5738837_G_C|
+    +----------+--------+----------+---------------+
+    <BLANKLINE>
+    """
+    w = Window.partitionBy(grouping_col).orderBy(sorting_col)
+    return get_top_ranked_in_window(df, w)
 
 
 def get_record_with_maximum_value(
     df: DataFrame, grouping_col: str, sorting_col: str
 ) -> DataFrame:
-    """Returns the record with the maximum value of the sorting column within each group of the grouping column."""
-    w = Window.partitionBy(grouping_col).orderBy(f.col(sorting_col).desc())
-    return (
-        df.withColumn("row_number", f.row_number().over(w))
-        .filter(f.col("row_number") == 1)
-        .drop("row_number")
-    )
-
-
-def get_gene_tss(strand_col: Column, start_col: Column, end_col: Column) -> Column:
-    """Returns the TSS of a gene based on its orientation.
+    """Returns the record with the maximum value of the sorting column within each group of the grouping column.
 
     Args:
-        strand_col (Column): Column containing 1 if the coding strand of the gene is forward, and -1 if it is reverse.
-        start_col (Column): Column containing the start position of the gene.
-        end_col (Column): Column containing the end position of the gene.
+        df (DataFrame): The DataFrame to be processed.
+        grouping_col (str): The column name(s) to group the DataFrame by.
+        sorting_col (str): The column name to sort the DataFrame by.
 
     Returns:
-        Column: Column containing the TSS of the gene.
+        DataFrame: The DataFrame with the record with the maximum value of the sorting column within each group of the grouping column.
 
     Examples:
     >>> df = spark.createDataFrame(
-    ...     [[1, 100, 150],
-    ...     [-1, 200, 250],
-    ...     [1, 300, 350]],
-    ...     ["strand", "start", "end"])
-    >>> df.withColumn(
-    ...     "tss",
-    ...     get_gene_tss(
-    ...         f.col("strand"),
-    ...         f.col("start"),
-    ...         f.col("end"),
+    ...     [[16, 5738837, "snv", "16_5738837_G_C"],
+    ...     [16, 5738892, "snv", "16_5738892_A_G"],
+    ...     [17, 5738922, "del", "16_5738922_CA_C"]],
+    ...     ["chromosome", "position", "alleleType", "id"])
+    >>> df.transform(
+    ...     lambda df: get_record_with_maximum_value(
+    ...         df, ["alleleType", "chromosome"], "position"
     ...     )
     ... ).show()
-    +------+-----+---+---+
-    |strand|start|end|tss|
-    +------+-----+---+---+
-    |     1|  100|150|100|
-    |    -1|  200|250|250|
-    |     1|  300|350|300|
-    +------+-----+---+---+
+    ++----------+--------+----------+---------------+
+    |chromosome|position|alleleType|             id|
+    +----------+--------+----------+---------------+
+    |        16| 5738892|       snv| 16_5738892_A_G|
+    |        17| 5738922|       del|16_5738922_CA_C|
+    +----------+--------+----------+---------------+
     <BLANKLINE>
-
     """
-    return f.when(strand_col == 1, start_col).when(strand_col == -1, end_col)
+    w = Window.partitionBy(grouping_col).orderBy(f.col(sorting_col).desc())
+    return get_top_ranked_in_window(df, w)
