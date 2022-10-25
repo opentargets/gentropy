6d204f92165978428fda236853ef4bf682ef2299
Irene López <irene.lopezs@protonmail.com>
2022-10-25 16:35:11 +0100

    test: added tests for spark helpers

--- src/etl/common/spark_helpers.py
+++ src/etl/common/spark_helpers.py
@@ -43,7 +43,9 @@ def get_top_ranked_in_window(df: DataFrame, w: WindowSpec) -> DataFrame:
 
 
 def get_record_with_minimum_value(
-    df: DataFrame, grouping_col: str, sorting_col: str
+    df: DataFrame,
+    grouping_col: Column | str | list[Column | str],
+    sorting_col: str,
 ) -> DataFrame:
     """Returns the record with the minimum value of the sorting column within each group of the grouping column.
 
@@ -54,32 +56,15 @@ def get_record_with_minimum_value(
 
     Returns:
         DataFrame: The DataFrame with the record with the minimum value of the sorting column within each group of the grouping column.
-
-    Examples:
-    >>> df = spark.createDataFrame(
-    ...     [[16, 5738837, "snv", "16_5738837_G_C"],
-    ...     [16, 5738892, "snv", "16_5738892_A_G"],
-    ...     [17, 5738922, "del", "16_5738922_CA_C"]],
-    ...     ["chromosome", "position", "alleleType", "id"])
-    >>> df.transform(
-    ...     lambda df: get_record_with_minimum_value(
-    ...         df, "chromosome", "position"
-    ...     )
-    ... ).show()
-    +----------+--------+----------+---------------+
-    |chromosome|position|alleleType|             id|
-    +----------+--------+----------+---------------+
-    |        17| 5738922|       del|16_5738922_CA_C|
-    |        16| 5738837|       snv| 16_5738837_G_C|
-    +----------+--------+----------+---------------+
-    <BLANKLINE>
     """
     w = Window.partitionBy(grouping_col).orderBy(sorting_col)
     return get_top_ranked_in_window(df, w)
 
 
 def get_record_with_maximum_value(
-    df: DataFrame, grouping_col: str, sorting_col: str
+    df: DataFrame,
+    grouping_col: Column | str | list[Column | str],
+    sorting_col: str,
 ) -> DataFrame:
     """Returns the record with the maximum value of the sorting column within each group of the grouping column.
 
@@ -90,25 +75,6 @@ def get_record_with_maximum_value(
 
     Returns:
         DataFrame: The DataFrame with the record with the maximum value of the sorting column within each group of the grouping column.
-
-    Examples:
-    >>> df = spark.createDataFrame(
-    ...     [[16, 5738837, "snv", "16_5738837_G_C"],
-    ...     [16, 5738892, "snv", "16_5738892_A_G"],
-    ...     [17, 5738922, "del", "16_5738922_CA_C"]],
-    ...     ["chromosome", "position", "alleleType", "id"])
-    >>> df.transform(
-    ...     lambda df: get_record_with_maximum_value(
-    ...         df, ["alleleType", "chromosome"], "position"
-    ...     )
-    ... ).show()
-    ++----------+--------+----------+---------------+
-    |chromosome|position|alleleType|             id|
-    +----------+--------+----------+---------------+
-    |        16| 5738892|       snv| 16_5738892_A_G|
-    |        17| 5738922|       del|16_5738922_CA_C|
-    +----------+--------+----------+---------------+
-    <BLANKLINE>
     """
     w = Window.partitionBy(grouping_col).orderBy(f.col(sorting_col).desc())
     return get_top_ranked_in_window(df, w)
---
+++ tests/test_spark_helpers.py
@@ -0,0 +1,48 @@
+"""Tests on helper spark functions."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pyspark.sql.functions as f
+import pytest
+
+from etl.common.spark_helpers import (
+    get_record_with_maximum_value,
+    get_record_with_minimum_value,
+)
+
+if TYPE_CHECKING:
+    from pyspark.sql import DataFrame, SparkSession
+
+
+@pytest.fixture
+def mock_variant_df(spark: SparkSession) -> DataFrame:
+    """Mock Dataframe with info of a variant ID."""
+    return spark.createDataFrame(
+        [
+            [16, 5738837, "snv", "16_5738837_G_C"],
+            [16, 10116, "del", "16_10116_TTG_T"],
+            [17, 5738922, "del", "16_5738922_CA_C"],
+        ],
+        ["chromosome", "position", "alleleType", "id"],
+    )
+
+
+def test_get_record_with_minimum_value_group_one_col(df: DataFrame) -> None:
+    """Test the util that return the row with the minimum value in a window by grouping per one column."""
+    grouping_col, sorting_col = "chromosome", "position"
+    df = df.transform(
+        lambda df: get_record_with_minimum_value(df, grouping_col, sorting_col)
+    )
+    assert (
+        df.filter(f.col("chromosome") == 16).collect()[0].__getitem__("position")
+    ), 10116
+
+
+def test_get_record_with_maximum_value_group_two_cols(df: DataFrame) -> None:
+    """Test the util that return the row with the minimum value in a window by grouping per two columns."""
+    grouping_col, sorting_col = ["chromosome", "alleleType"], "position"
+    df = df.transform(
+        lambda df: get_record_with_maximum_value(df, grouping_col, sorting_col)
+    )
+    assert df.count(), 3
