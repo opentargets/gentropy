f3c746223e84629a8b952f6000a9a15df18834b0
Daniel Suveges <daniel.suveges@protonmail.com>
2022-11-28 10:20:47 +0000

    fix: slight updates

--- src/run_gwas_ingest.py
+++ src/run_gwas_ingest.py
@@ -11,6 +11,15 @@ if TYPE_CHECKING:
 
 from etl.common.ETLSession import ETLSession
 from etl.gwas_ingest.pics import pics_all_study_locus
+from etl.gwas_ingest.process_associations import (
+    ingest_gwas_catalog_associations,
+    prepare_associations_for_pics,
+)
+from etl.gwas_ingest.study_ingestion import (
+    generate_study_table,
+    ingest_gwas_catalog_studies,
+    spliting_gwas_studies,
+)
 
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
@@ -19,65 +28,59 @@ def main(cfg: DictConfig) -> None:
     etl = ETLSession(cfg)
     hl.init(sc=etl.spark.sparkContext, default_reference="GRCh38")
 
-    # etl.logger.info("Ingesting GWAS Catalog association data...")
-    #
-    # # Ingesting GWAS Catalog associations:
-    # assoc = ingest_gwas_catalog_associations(
-    #     etl,
-    #     cfg.etl.gwas_ingest.inputs.gwas_catalog_associations,
-    #     cfg.etl.variant_annotation.outputs.variant_annotation,
-    #     cfg.etl.gwas_ingest.parameters.p_value_cutoff,
+    etl.logger.info("Ingesting GWAS Catalog association data...")
+
+    # Ingesting GWAS Catalog associations:
+    assoc = ingest_gwas_catalog_associations(
+        etl,
+        cfg.etl.gwas_ingest.inputs.gwas_catalog_associations,
+        cfg.etl.variant_annotation.outputs.variant_annotation,
+        cfg.etl.gwas_ingest.parameters.p_value_cutoff,
+    )
+
+    etl.logger.info(
+        f"Writing associations data to: {cfg.etl.gwas_ingest.outputs.gwas_catalog_associations}"
+    )
+    (
+        assoc.write.mode(cfg.environment.sparkWriteMode).parquet(
+            cfg.etl.gwas_ingest.outputs.gwas_catalog_associations
+        )
+    )
+    etl.logger.info("Ingesting GWAS Catalog Studies...")
+
+    # # Read saved association data:
+    # assoc = etl.spark.read.parquet(
+    #     "gs://genetics_etl_python_playground/XX.XX/output/python_etl/parquet/gwas_catalog_associations_patched"
     # )
 
-    # etl.logger.info(
-    #     f"Writing associations data to: {cfg.etl.gwas_ingest.outputs.gwas_catalog_associations}"
-    # )
-    # (
-    #     assoc.write.mode(cfg.environment.sparkWriteMode).parquet(
-    #         cfg.etl.gwas_ingest.outputs.gwas_catalog_associations
-    #     )
-    # )
-    # etl.logger.info("Ingesting GWAS Catalog Studies...")
+    # Ingesting GWAS Catalog studies:
+    gwas_studies = ingest_gwas_catalog_studies(
+        etl,
+        cfg.etl.gwas_ingest.inputs.gwas_catalog_studies,
+        cfg.etl.gwas_ingest.inputs.gwas_catalog_ancestries,
+        cfg.etl.gwas_ingest.inputs.summary_stats_list,
+    )
 
-    # # # Read saved association data:
-    # # assoc = etl.spark.read.parquet(
-    # #     "gs://genetics_etl_python_playground/XX.XX/output/python_etl/parquet/gwas_catalog_associations_patched"
-    # # )
+    # Joining study and association
+    study_assoc = assoc.join(gwas_studies, on="studyAccession", how="outer").transform(
+        spliting_gwas_studies
+    )
 
-    # # Ingesting GWAS Catalog studies:
-    # gwas_studies = ingest_gwas_catalog_studies(
-    #     etl,
-    #     cfg.etl.gwas_ingest.inputs.gwas_catalog_studies,
-    #     cfg.etl.gwas_ingest.inputs.gwas_catalog_ancestries,
-    #     cfg.etl.gwas_ingest.inputs.summary_stats_list,
-    # )
-    # etl.logger.info(
-    #     f"Writing studies data to: {cfg.etl.gwas_ingest.outputs.gwas_catalog_studies}"
-    # )
-    # gwas_studies.write.mode(cfg.environment.sparkWriteMode).parquet(
-    #     cfg.etl.gwas_ingest.outputs.gwas_catalog_studies
-    # )
+    # Extracting study table and save:
+    (
+        study_assoc.transform(generate_study_table)
+        .write.mode(cfg.environment.sparkWriteMode)
+        .parquet(cfg.etl.gwas_ingest.outputs.gwas_catalog_studies)
+    )
 
-    # # Joining study and association
-    # study_assoc = assoc.join(gwas_studies, on="studyAccession", how="outer").transform(
-    #     spliting_gwas_studies
-    # )
-
-    # # Extracting study table and save:
-    # (
-    #     study_assoc.transform(generate_study_table)
-    #     .write.mode(cfg.environment.sparkWriteMode)
-    #     .parquet(cfg.etl.gwas_ingest.outputs.gwas_catalog_studies)
-    # )
-
-    # # Extracting associations for PICS and save:
-    # (
-    #     study_assoc.transform(prepare_associations_for_pics)
-    #     .write.mode(cfg.environment.sparkWriteMode)
-    #     .parquet(
-    #         "gs://genetics_etl_python_playground/XX.XX/output/python_etl/parquet/gwas_catalog_PICS_ready"
-    #     )
-    # )
+    # Extracting associations for PICS and save:
+    (
+        study_assoc.transform(prepare_associations_for_pics)
+        .write.mode(cfg.environment.sparkWriteMode)
+        .parquet(
+            "gs://genetics_etl_python_playground/XX.XX/output/python_etl/parquet/gwas_catalog_PICS_ready"
+        )
+    )
 
     pics_ready = etl.spark.read.parquet(
         "gs://genetics_etl_python_playground/XX.XX/output/python_etl/parquet/gwas_catalog_PICS_ready"
