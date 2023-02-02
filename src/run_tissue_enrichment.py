e4f03d67752ba1b7c11070c2110fe9968a18c9aa
xyg123 <xiangyuge123@hotmail.co.uk>
2023-02-02 16:57:07 +0000

    feat: removed liftover, reduced code smell

--- src/run_tissue_enrichment.py
+++ src/run_tissue_enrichment.py
@@ -7,16 +7,10 @@ from typing import TYPE_CHECKING
 import hydra
 
 from etl.common.ETLSession import ETLSession
-from etl.tissue_enrichment.tissue_enrichment import cheers
-from etl.tissue_enrichment.utils import load_peaks, load_snps
+from etl.tissue_enrichment.tissue_enrichment import cheers, load_peaks
 
 if TYPE_CHECKING:
     from omegaconf import DictConfig
-
-# Import required functions from src :
-# from etl.common.ETLSession import ETLSession
-# from etl.json import validate_df_schema
-# from etl.variants.variant_annotation import generate_variant_annotation
 
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
@@ -27,38 +21,34 @@ def main(cfg: DictConfig) -> None:
 
     # Loading data
 
-    summary_stats_credible_set = load_snps(
-        etl.spark, cfg.etl.tissue_enrichment.inputs.summary_stats_credible_set
+    summary_stats_credible_set = etl.spark.read.format("parquet").load(
+        cfg.etl.tissue_enrichment.inputs.summary_stats_credible_set
     )
 
-    #    pics_credible_set = load_snps(
-    #        etl.spark, cfg.etl.tissue_enrichment.inputs.pics_credible_set
-    #    )
+    pics_credible_set = etl.spark.read.format("parquet").load(
+        cfg.etl.tissue_enrichment.inputs.pics_credible_set
+    )
     tissue_annotations = load_peaks(
         etl.spark, cfg.etl.tissue_enrichment.inputs.tissue_annotations
     )
 
-    etl.logger.info("Computing tissue enrichment calculations...")
-
-    summary_stats_enrichment = cheers(tissue_annotations, summary_stats_credible_set)
-
-    #    pics_enrichment = cheers(
-    #        tissue_annotations,
-    #        pics_credible_set,
-    #    )
-
-    etl.logger.info("Writing tissue enrichment results...")
-    (
-        summary_stats_enrichment.write.mode(cfg.environment.sparkWriteMode).parquet(
-            cfg.etl.tissue_enrichment.outputs.summary_stats_enrichment
-        )
+    summary_stats_enrichment = cheers(
+        tissue_annotations,
+        summary_stats_credible_set,
     )
 
-    #    (
-    #        pics_enrichment.write.mode(cfg.environment.sparkWriteMode).parquet(
-    #            cfg.etl.tissue_enrichment.outputs.pics_enrichment
-    #        )
-    #    )
+    pics_enrichment = cheers(
+        tissue_annotations,
+        pics_credible_set,
+    )
+
+    summary_stats_enrichment.write.mode(cfg.environment.sparkWriteMode).parquet(
+        cfg.etl.tissue_enrichment.outputs.summary_stats_enrichment
+    )
+
+    pics_enrichment.write.mode(cfg.environment.sparkWriteMode).parquet(
+        cfg.etl.tissue_enrichment.outputs.pics_enrichment
+    )
 
     etl.logger.info("Tissue enrichment finished")
 
