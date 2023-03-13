180187db5d2e276aad35a351e60d6e9f6032e32a
Irene López <irene.lopezs@protonmail.com>
2023-03-13 15:29:47 +0100

    Merge branch 'do_hydra' of https://github.com/opentargets/genetics_etl_python into il-l2g-prototype

--- src/otg/l2g.py
+++ src/otg/l2g.py
@@ -4,40 +4,14 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-import hydra
-import pyspark.sql.functions as f
-
 if TYPE_CHECKING:
     from omegaconf import DictConfig
-    from pyspark.sql import DataFrame
 
 from otg.common.session import Session
-from otg.common.spark_helpers import get_record_with_maximum_value
 from otg.config.l2g import LocusToGeneConfig
-from otg.dataset.l2g_feature_matrix import L2G, L2GFeatureMatrix
-from otg.dataset.study_locus import StudyLocus
-from otg.dataset.study_locus_overlap import StudyLocusOverlap
-from otg.dataset.v2g import V2G
+from otg.dataset.l2g_feature_matrix import L2GFeatureMatrix
+from otg.dataset.l2g_gold_standard import L2GGoldStandard
 from otg.method.locus_to_gene import LocusToGeneTrainer
-
-
-@hydra.main(version_base=None, config_path=".", config_name="config")
-def main(cfg: DictConfig) -> None:
-    """Run Locus to Gene."""
-    etl = Session(cfg)
-
-    if cfg.run_mode == "train":
-        gold_standards = get_gold_standards(
-            etl,
-            cfg.gold_standard.curation,
-            cfg.feature_inputs.v2g,
-            cfg.feature_inputs.study_locus,
-            cfg.feature_inputs.study_locus_overlap,
-            cfg.gold_standard.interactions,
-        )
-        fm = L2GFeatureMatrix  # TODO: inverse matrix
-        data = gold_standards.join(fm, on="studyLocusId", how="inner").train_test_split(frac=0.1, seed=42)
-        # TODO: data normalization and standardisation of features
 
 
 @dataclass
@@ -61,19 +35,6 @@ class LocusToGeneStep(LocusToGeneConfig):
         """Run Locus to Gene step."""
         self.etl.logger.info(f"Executing {self.id} step")
 
-        if self.run_mode == "train":
-            gold_standards = get_gold_standards(
-                etl=self.etl,
-                gold_standard_curation=self.l2g_curation.path,
-                v2g_path=self.v2g.path,
-                study_locus_path=self.study_locus.path,
-                study_locus_overlap_path=self.study_locus_overlap.path,
-                interactions_path=self.gene_interactions.path,
-            )
-            fm = L2GFeatureMatrix  # TODO: inverse matrix
-            data = gold_standards.join(fm, on="studyLocusId", how="inner").train_test_split(frac=0.1, seed=42)
-            # TODO: data normalization and standardisation of features
-
         if self.cfg.run_mode == "train":
             gold_standards = L2GGoldStandard.get_gold_standards(
                 etl=self.etl,
@@ -86,102 +47,20 @@ class LocusToGeneStep(LocusToGeneConfig):
             #     gold_standards = self.etl.spark.read.parquet(
             #         "/Users/irenelopez/MEGAsync/EBI/repos/genetics_etl_python/mock_data/processed_gs"
             #     )
-            fm = L2GFeatureMatrix  # FIXME: debug credset
-            data = gold_standards.join(fm, on="studyLocusId", how="inner").train_test_split(frac=0.1, seed=42)
+            fm = L2GFeatureMatrix.generate_features(
+                etl=self.etl,
+                study_locus_path=self.cfg.study_locus_path,
+                study_index_path=self.cfg.study_index_path,
+                variant_gene_path=self.cfg.variant_gene_path,
+                colocalisation_path=self.cfg.colocalisation_path,
+            )
+            train, test = gold_standards.join(fm, on="studyLocusId", how="inner").train_test_split(fraction=0.8)
             # TODO: data normalization and standardisation of features
 
-        #     LocusToGeneTrainer.train(
-        #         train_set=data["train"],
-        #         test_set=data["test"],
-        #         **self.hyperparameters,
-        #         # TODO: Add push to hub, and push to W&B
-        #     )
-
-
-def get_gold_standards(
-    etl: Session,
-    gold_standard_curation: str,
-    v2g_path: str,
-    study_locus_path: str,
-    study_locus_overlap_path: str,
-    interactions_path: str,
-) -> L2G:
-    """Process gold standard curation to use as training data."""
-    # FIXME: assign function to class
-    overlaps_df = StudyLocusOverlap.from_parquet(study_locus_overlap_path).select(
-        "left_studyLocusId", "right_studyLocusId"
-    )
-    interactions = process_gene_interactions(etl, interactions_path)
-    return (
-        etl.spark.read.parquet(gold_standard_curation)
-        .select(
-            f.col("association_info.otg_id").alias("studyId"),
-            f.col("gold_standard_info.gene_id").alias("geneId"),
-            f.concat_ws(
-                "_",
-                f.col("sentinel_variant.locus_GRCh38.chromosome"),
-                f.col("sentinel_variant.locus_GRCh38.position"),
-                f.col("sentinel_variant.alleles.reference"),
-                f.col("sentinel_variant.alleles.alternative"),
-            ).alias("variantId"),
+        LocusToGeneTrainer.train(
+            train_set=train,
+            test_set=test,
+            track=self.track,
+            **self.hyperparameters,
+            # TODO: Add push to hub
         )
-        .filter(f.col("gold_standard_info.highest_confidence").isin(["High", "Medium"]))
-        # Bring studyLocusId - TODO: what if I don't have one?
-        .join(
-            StudyLocus.from_parquet(study_locus_path).select("studyId", "variantId", "studyLocusId"),
-            on=["studyId", "variantId"],
-            how="inner",
-        )
-        # Assign Positive or Negative Status based on confidence
-        .join(
-            V2G.from_parquet(v2g_path).select("variantId", "geneId", "distance"),
-            on=["variantId", "geneId"],
-            how="inner",
-        )
-        .withColumn(
-            "gsStatus",
-            f.when(f.col("distance") <= 500_000, "Positive").otherwise("Negative"),
-        )
-        # Remove redundant loci
-        .alias("left")
-        .join(
-            overlaps_df.alias("right"),
-            (f.col("left.variantId") == f.col("right.left_studyLocusId"))
-            | (f.col("left.variantId") == f.col("right.right_studyLocusId")),
-            how="left",
-        )
-        .distinct()
-        # Remove redundant genes
-        .join(
-            interactions.alias("interactions"),
-            (f.col("left.geneId") == f.col("interactions.geneIdA"))
-            | (f.col("left.geneId") == f.col("interactions.geneIdB")),
-            how="left",
-        )
-        .withColumn("interacting", (f.col("scoring") > 0.7))
-        # filter out genes where geneIdA has gsStatus Negative but geneIdA and gene IdB are interacting
-        .filter(
-            ~(
-                (f.col("gsStatus") == "Negative")
-                & (f.col("interacting"))
-                & (
-                    (f.col("left.geneId") == f.col("interactions.geneIdA"))
-                    | (f.col("left.geneId") == f.col("interactions.geneIdB"))
-                )
-            )
-        )
-    )
-
-
-def process_gene_interactions(etl: Session, interactions_path: str) -> DataFrame:
-    """Extract top scoring gene-gene interaction from the Platform."""
-    # FIXME: assign function to class
-    return get_record_with_maximum_value(
-        etl.spark.read.parquet(interactions_path),
-        ["targetA", "targetB"],
-        "scoring",
-    ).selectExpr(
-        "targetA as geneIdA",
-        "targetB as geneIdB",
-        "scoring as score",
-    )
--- src/otg/method/locus_to_gene.py
+++ src/otg/method/locus_to_gene.py
@@ -5,29 +5,29 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+import wandb
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from xgboost import plot_importance as xgb_plot_importance
-from xgboost.spark import SparkXGBRegressor
+from xgboost.spark import SparkXGBClassifier
 
 if TYPE_CHECKING:
+    from pyspark.sql import DataFrame
+
     from otg.dataset.l2g_feature_matrix import L2GFeatureMatrix
 
 
-class LocusToGeneModel(SparkXGBRegressor):
+@dataclass
+class LocusToGeneModel(SparkXGBClassifier):
     """Wrapper for the Locus to Gene classifier."""
 
     label_col: str
     missing: float
     n_estimators: int
 
-    def __init__(self: LocusToGeneModel) -> None:
-        """TBC."""
-        super().__init__()
-
     def save(self: LocusToGeneModel, path: str) -> None:
-        """TBC."""
-        pass
-
-    def evaluate(self: LocusToGeneModel, test_df: L2GFeatureMatrix) -> None:
         """TBC."""
         pass
 
@@ -51,15 +51,13 @@ class LocusToGenePredictor:
         cls: type[LocusToGenePredictor], model_path: str
     ) -> LocusToGenePredictor:
         """Load a model from a given path."""
-        return cls(model=SparkXGBRegressor.load(model_path))
+        return cls(model=SparkXGBClassifier.load(model_path))
 
     def predict(
         self: LocusToGenePredictor, test_df: L2GFeatureMatrix
-    ) -> L2GFeatureMatrix:  # FIXME: Review
+    ) -> DataFrame:  # FIXME: Review
         """Apply the model to a given L2GFeatureMatrix dataset."""
-        # model: LocusToGeneModel = hydra.utils.instantiate(cfg.model)
-
-        return self.model.transform(test_df)
+        return self.model.transform(test_df._df)
 
     def evaluate(self: LocusToGenePredictor) -> None:
         """TBC."""
@@ -69,33 +67,9 @@ class LocusToGenePredictor:
 class LocusToGeneTrainer:
     """Modelling of what is the most likely causal gene associated with a given locus."""
 
-    @classmethod
-    def train(
-        cls: type[LocusToGeneTrainer],
-        model: LocusToGeneModel,
-        train_df: L2GFeatureMatrix,
-    ) -> LocusToGeneModel:
-        """Train the Locus to Gene model.
-
-        Args:
-            model (LocusToGeneModel): Model to train
-            train_df (L2GFeatureMatrix): Training data
-
-        Returns:
-            LocusToGeneModel: Trained model
-        """
-        # TODO: Read config from hydra to parametrise the model
-        model.fit(train_df)
-        return model
-
-    def k_fold_cross_validation(self: LocusToGeneTrainer) -> None:
-        """TBC."""
-        # See this demo: https://xgboost.readthedocs.io/en/stable/python/examples/cross_validation.html#sphx-glr-python-examples-cross-validation-py
-        pass
-
-    def evaluate(self: LocusToGeneTrainer) -> None:
-        """TBC."""
-        """
+    @staticmethod
+    def evaluate(test_set: DataFrame, wandb_project: str = "otg_l2g") -> None:
+        """Perform evaluation of the model by tracking ."""
         evaluator = BinaryClassificationEvaluator(
             labelCol="label",
             rawPredictionCol="rawPrediction",
@@ -103,11 +77,80 @@ class LocusToGeneTrainer:
         )
         evaluator.setRawPredictionCol("prediction")
         run = wandb.init(
-            project="otg-l2g", job_type="binary-class-xgboost-logloss", save_code=False
-        )  # TODO: parametrise all this
-        wand_evaluator = WandbEvaluator(sparkMlEvaluator=evaluator, run=run) # FIXME: this class is here https://github.com/timsetsfire/wandb-spark/blob/main/wandb/spark/evaluation.py
+            project=wandb_project,
+            job_type="binary-class-xgboost-logloss",
+            save_code=False,
+        )
+        wandb_evaluator = wandb.WandbEvaluator(sparkMlEvaluator=evaluator, run=run)
         wandb_evaluator.setWandbRun(run)
         wandb_evaluator.setMetricPrefix("test/")
-        wandb_evaluator.evaluate(dataset)
+        wandb_evaluator.evaluate(test_set)
         run.finish()
+
+    @classmethod
+    def train(
+        cls: type[LocusToGeneTrainer],
+        model: LocusToGeneModel,
+        train_df: L2GFeatureMatrix,
+        track: bool,
+    ) -> LocusToGeneModel:
+        """Train the Locus to Gene model.
+
+        Args:
+            model (LocusToGeneModel): Model to train
+            train_df (L2GFeatureMatrix): Training data
+            track (bool): Whether to track the training process with wandb
+
+        Returns:
+            LocusToGeneModel: Trained model
         """
+        # TODO: Read config from hydra to parametrise the model
+        train, eval = train_df.randomSplit([0.8, 0.2])
+        model = model.fit(train)
+        if track:
+            LocusToGeneTrainer.evaluate(test_set=model.transform(eval))
+        return model
+
+    def k_fold_cross_validation(
+        self: LocusToGeneTrainer, data: L2GFeatureMatrix
+    ) -> None:
+        """TBC."""
+        # See this demo: https://xgboost.readthedocs.io/en/stable/python/examples/cross_validation.html#sphx-glr-python-examples-cross-validation-py
+        feature_cols = data._df.drop("label").columns
+        assembler = VectorAssembler(inputCols=feature_cols, outputCol="label")
+        data = assembler.transform(data)
+
+        train_data, test_data = data.train_test_split(0.8)
+
+        # Define model
+        xgb = SparkXGBClassifier(
+            objective="binary:logistic",
+            eval_metric="logloss",
+            num_round=10,
+            num_workers=4,
+            featuresCol="features",
+            labelCol="label",
+        )
+        param_grid = (
+            ParamGridBuilder()
+            .addGrid(xgb.maxDepth, [2, 4, 6])
+            .addGrid(xgb.eta, [0.01, 0.1, 0.3])
+            .build()
+        )
+
+        # Define evaluator - TODO: use my custom one
+        evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
+
+        # Define cross validator
+        cross_validator = CrossValidator(
+            estimator=xgb,
+            estimatorParamMaps=param_grid,
+            evaluator=evaluator,
+            numFolds=5,
+        )
+
+        # set all stages in the pipeline
+        pipeline = Pipeline(stages=[assembler, xgb, cross_validator])
+        predictions = pipeline.fit(train_data).transform(test_data)
+
+        evaluator(predictions)
