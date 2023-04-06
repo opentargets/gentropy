829ef6b982cc7c83c9fa95928140d23987cd67d3
Irene López <irene.lopezs@protonmail.com>
2023-04-06 20:18:22 +0100

    fix: join `_variant_coordinates_in_ldindex` on `variantId`
    variant_df does not have split coordinates

--- src/otg/method/ld.py
+++ src/otg/method/ld.py
@@ -91,7 +91,7 @@ class LDAnnotatorGnomad:
         return (
             variants_df.join(
                 ld_index.df,
-                on=["chromosome", "position", "referenceAllele", "alternateAllele"],
+                on=["variantId", "chromosome"],
             )  # start idx > stop idx in rare occasions due to liftover
             .filter(f.col("start_idx") < f.col("stop_idx"))
             .groupBy("chromosome", "idx")
