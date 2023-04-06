ffe825ffd952bf69422191ab08c925287945b137
Irene López <irene.lopezs@protonmail.com>
2023-04-06 18:37:24 +0100

    fix: l2g tests fix, more samples added so that splitting doesnt fail

--- tests/method/test_locus_to_gene.py
+++ tests/method/test_locus_to_gene.py
@@ -25,32 +25,37 @@ def mock_training_data(spark: SparkSession) -> L2GFeatureMatrix:
             (4.0, 5.0, "positive"),
             (7.0, 8.0, "negative"),
             (10.0, 11.0, "positive"),
+            (13.0, 14.0, "negative"),
+            (16.0, 17.0, "positive"),
         ],
         ["feature_1", "feature_2", "gold_standard_set"],
     )
     return L2GFeatureMatrix(_df=df)
 
 
+@pytest.fixture(scope="module")
+def model() -> LocusToGeneModel:
+    """Creates an instance of the LocusToGene class."""
+    estimator = LogisticRegression(featuresCol="features", labelCol="label")
+    return LocusToGeneModel(
+        _estimator=estimator, features_list=["feature_1", "feature_2"]
+    )
+
+
 class TestLocusToGeneTrainer:
     """Test the L2GTrainer methods using a logistic regression model as estimation algorithm."""
-
-    def __init__(self: TestLocusToGeneTrainer) -> None:
-        """Constructor for the TestLocusToGeneTrainer class."""
-        self.estimator = LogisticRegression(featuresCol="features", labelCol="label")
-        self.model = LocusToGeneModel(
-            _estimator=self.estimator, features_list=["feature_1", "feature_2"]
-        )
 
     def test_cross_validate(
         self: TestLocusToGeneTrainer,
         mock_training_data: L2GFeatureMatrix,
+        model: LocusToGeneModel,
     ) -> None:
         """Test the k-fold cross-validation function with a logistic regression model."""
         param_grid = (
-            ParamGridBuilder().addGrid(self.estimator.regParam, [0.1, 0.01]).build()
+            ParamGridBuilder().addGrid(model._estimator.regParam, [0.1, 0.01]).build()
         )
         best_model = LocusToGeneTrainer.cross_validate(
-            self.model, mock_training_data, num_folds=2, param_grid=param_grid
+            model, mock_training_data, num_folds=2, param_grid=param_grid
         )
 
         # Check that the best model is trained with the optimal hyperparameters
@@ -60,9 +65,10 @@ class TestLocusToGeneTrainer:
     def test_train(
         self: TestLocusToGeneTrainer,
         mock_training_data: L2GFeatureMatrix,
+        model: LocusToGeneModel,
     ) -> None:
         """Test the training function with a logistic regression model."""
-        trained_model = LocusToGeneTrainer.train(mock_training_data, self.model)
+        trained_model = LocusToGeneTrainer.train(mock_training_data, model)
 
         # Check that `model` is a PipelineModel object and not None
         assert isinstance(trained_model.model, PipelineModel)
