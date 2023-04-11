5a8e03a7030c88b6b7c39f2ed20123598ff76dea
Irene López <irene.lopezs@protonmail.com>
2023-04-11 10:22:35 +0200

    fix: order ld index by idx and unpersist data

--- src/otg/method/ld.py
+++ src/otg/method/ld.py
@@ -232,9 +232,11 @@ class LDAnnotatorGnomad:
             DataFrame: LD information in the columns ["variantId", "chromosome", "gnomadPopulation", "tagVariantId", "r"]
         """
         # map variants to precomputed LD indexes from gnomAD
-        variants_ld_coordinates = LDAnnotatorGnomad._variant_coordinates_in_ldindex(
-            variants_df, ld_index
-        ).persist()
+        variants_ld_coordinates = (
+            LDAnnotatorGnomad._variant_coordinates_in_ldindex(variants_df, ld_index)
+            .sort(f.col("idx"))
+            .persist()
+        )
 
         # idxs for lead, first variant in the region and last variant in the region
         entries = LDAnnotatorGnomad._query_block_matrix(
@@ -245,7 +247,7 @@ class LDAnnotatorGnomad:
             min_r2,
         )
 
-        return (
+        variants_with_ld = (
             entries.join(
                 f.broadcast(variants_ld_coordinates),
                 on="i",
@@ -268,6 +270,8 @@ class LDAnnotatorGnomad:
                 "variantId", "leads.chromosome", "gnomadPopulation", "tagVariantId", "r"
             )
         )
+        variants_ld_coordinates.unpersist()
+        return variants_with_ld
 
     @classmethod
     def ld_annotation_by_locus_ancestry(
