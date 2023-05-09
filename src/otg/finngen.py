7a4a8d6a948208bfd8def728e9661ecdfa87f5a9
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-05-09 13:58:13 +0100

    style: reorganise individual function calls into a concatenated chain of calls

--- src/otg/finngen.py
+++ src/otg/finngen.py
@@ -31,38 +31,39 @@ class FinnGenStep(FinnGenStepConfig):
         # Read the JSON data from the URL.
         json_data = urlopen(self.finngen_phenotype_table_url).read().decode("utf-8")
         rdd = self.session.spark.sparkContext.parallelize([json_data])
-        df = self.session.spark.read.json(rdd)
-
-        # Select the desired columns.
-        df = df.select("phenocode", "phenostring", "num_cases", "num_controls")
-
-        # Rename the columns.
-        df = df.withColumnRenamed("phenocode", "studyId")
-        df = df.withColumnRenamed("phenostring", "traitFromSource")
-        df = df.withColumnRenamed("num_cases", "nCases")
-        df = df.withColumnRenamed("num_controls", "nControls")
-
-        # Transform the column values.
-        df = df.withColumn(
-            "studyId", f.concat(f.lit(self.finngen_release_prefix), df["studyId"])
+        df = (
+            self.session.spark.read.json(rdd)
+            # Select the desired columns.
+            .select("phenocode", "phenostring", "num_cases", "num_controls")
+            # Rename the columns.
+            .withColumnRenamed("phenocode", "studyId")
+            .withColumnRenamed("phenostring", "traitFromSource")
+            .withColumnRenamed("num_cases", "nCases")
+            .withColumnRenamed("num_controls", "nControls")
+            # Transform the column values.
+            .withColumn(
+                "studyId",
+                f.concat(f.lit(self.finngen_release_prefix), f.col("studyId")),
+            )
+            .withColumn("nSamples", f.col("nCases") + f.col("nControls"))
+            .withColumn(
+                "summarystatsLocation",
+                f.concat(
+                    f.lit(self.finngen_sumstat_url_prefix),
+                    f.col("studyId"),
+                    f.lit(self.finngen_sumstat_url_suffix),
+                ),
+            )
+            # Set constant value columns.
+            # Then f.when(f.lit(True)) trick makes sure that the column is created as nullable, to ensure that it is not flagged as incorrect by validate_df_schema. See: https://stackoverflow.com/a/68578278.
+            .withColumn(
+                "initialSampleSize",
+                f.when(
+                    f.lit(True), f.lit("342,499 (190,879 females and 151,620 males)")
+                ),
+            )
+            .withColumn("hasSumstats", f.when(f.lit(True), f.lit(True)))
         )
-        df = df.withColumn("nSamples", df["nCases"] + df["nControls"])
-        df = df.withColumn(
-            "summarystatsLocation",
-            f.concat(
-                f.lit(self.finngen_sumstat_url_prefix),
-                df["studyId"],
-                f.lit(self.finngen_sumstat_url_suffix),
-            ),
-        )
-
-        # Set constant value columns.
-        # Then f.when(f.lit(True)) trick makes sure that the column is created as nullable, to ensure that it is not flagged as incorrect by validate_df_schema. See: https://stackoverflow.com/a/68578278.
-        df = df.withColumn(
-            "initialSampleSize",
-            f.when(f.lit(True), f.lit("342,499 (190,879 females and 151,620 males)")),
-        )
-        df = df.withColumn("hasSumstats", f.when(f.lit(True), f.lit(True)))
 
         # Write the output.
         df.write.mode(self.session.write_mode).parquet(self.finngen_study_index_out)
