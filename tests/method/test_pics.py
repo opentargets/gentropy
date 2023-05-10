56c782ae58e25efbf21b385cab6cd2a8d5027711
Irene López <irene.lopezs@protonmail.com>
2023-05-10 19:39:27 +0100

    test: add `test_finemap_null_r2` (fails)

--- tests/method/test_pics.py
+++ tests/method/test_pics.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import pyspark.sql.functions as f
+import pytest
 from pyspark.sql.types import ArrayType
 
 from otg.dataset.study_locus import StudyLocus
@@ -26,6 +27,34 @@ class TestFinemap:
             f.rand().alias("neglog_pvalue"),
             f.array().alias("credibleSet"),
         )
+        observed = (
+            df.select(self._finemap_udf_expr.alias("credibleSet"))
+            .limit(1)
+            .collect()[0]["credibleSet"]
+        )
+
+        assert observed == []
+
+    def test_finemap_null_r2(self: TestFinemap, mock_study_locus: StudyLocus) -> None:
+        """Test finemap works when `r2Overall` is null by returning the same `credibleSet` content."""
+        df = mock_study_locus.df.withColumn(
+            "credibleSet",
+            f.transform(
+                "credibleSet",
+                lambda x: x.withField("r2Overall", f.lit(None)),
+            ),
+        ).select("variantId", f.rand().alias("neglog_pvalue"), "credibleSet")
+
+        observed = (
+            df.select(self._finemap_udf_expr.alias("credibleSet"))
+            .limit(1)
+            .collect()[0]["credibleSet"]
+        )
+        print(observed)  # failing, nonetype is not iterable
+
+    @pytest.fixture(autouse=True)
+    def _setup(self: TestFinemap, mock_study_locus: StudyLocus) -> None:
+        """Prepares common fixtures for the tests."""
         credset_schema = ArrayType(
             [
                 field.dataType.elementType  # type: ignore
@@ -37,16 +66,7 @@ class TestFinemap:
             lambda credible_set, neglog_p: PICS._finemap(credible_set, neglog_p),
             credset_schema,
         )
-        observed = (
-            df.withColumn(
-                "credibleSet",
-                f.when(
-                    f.size("credibleSet") > 0,
-                    _finemap_udf(f.col("credibleSet"), f.col("neglog_pvalue")),
-                ).otherwise(f.col("credibleSet")),
-            )
-            .limit(1)
-            .collect()[0]["credibleSet"]
-        )
-
-        assert observed == []
+        self._finemap_udf_expr = f.when(
+            f.size("credibleSet") > 0,
+            _finemap_udf(f.col("credibleSet"), f.col("neglog_pvalue")),
+        ).otherwise(f.col("credibleSet"))
