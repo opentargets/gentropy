6a78782a951f6824168dd7b824d91faa9ba09f49
hlnicholls <53306752+hlnicholls@users.noreply.github.com>
2023-07-11 12:50:07 +0100

    feat: added from_ukbiobank_summary_stats test

--- tests/dataset/test_summary_statistics.py
+++ tests/dataset/test_summary_statistics.py
@@ -39,3 +39,31 @@ def test_summary_statistics__calculate_confidence_interval__new_columns(
     # These two columns are computed by the tested function:
     assert "betaConfidenceIntervalLower" in columns
     assert "betaConfidenceIntervalUpper" in columns
+
+
+def test_summary_statistics__from_ukbiobank_summary_stats(
+    ukbb_mock_summary_statistics: SummaryStatistics,
+) -> None:
+    """Test if UKBiobank summary statistics are correctly ingested."""
+    ukbiobank_study_id = "NEALE2_100001_raw"
+    result = SummaryStatistics.from_ukbiobank_summary_stats(
+        ukbb_mock_summary_statistics, ukbiobank_study_id
+    )
+
+    # Check if the variantId column is created correctly
+    expected_variant_ids = [
+        "1_758351_A_G",
+        "1_909894_G_T",
+        "1_933024_C_T",
+        "1_973673_G_A",
+        "1_1037956_A_G",
+        "1_1055604_G_A",
+        "1_1065477_G_A",
+        "1_1065797_G_C",
+        "1_1148447_T_C",
+        "1_1156196_A_C",
+    ]
+
+    assert (
+        result.df.filter(result.df.variantId.isin(expected_variant_ids)).count() == 10
+    )
