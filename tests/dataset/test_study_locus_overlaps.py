2b25284390496e8cdc7073ba78ee268f12a650aa
Irene López <irene.lopezs@protonmail.com>
2023-08-22 20:01:02 +0100

    test: added `test_study_locus_overlap_from_associations`

--- tests/dataset/test_study_locus_overlaps.py
+++ tests/dataset/test_study_locus_overlaps.py
@@ -1,7 +1,13 @@
 """Test colocalisation dataset."""
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from otg.dataset.study_locus_overlap import StudyLocusOverlap
+
+if TYPE_CHECKING:
+    from otg.dataset.study_index import StudyIndex
+    from otg.dataset.study_locus import StudyLocus
 
 
 def test_study_locus_overlap_creation(
@@ -9,3 +15,11 @@ def test_study_locus_overlap_creation(
 ) -> None:
     """Test colocalisation creation with mock data."""
     assert isinstance(mock_study_locus_overlap, StudyLocusOverlap)
+
+
+def test_study_locus_overlap_from_associations(
+    mock_study_locus: StudyLocus, mock_study_index: StudyIndex
+) -> None:
+    """Test colocalisation creation from mock associations."""
+    overlaps = StudyLocusOverlap.from_associations(mock_study_locus, mock_study_index)
+    assert isinstance(overlaps, StudyLocusOverlap)
