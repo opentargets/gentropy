0c68bad3fef232591ade9aaa685023150a74260c
David Ochoa <dogcaesar@gmail.com>
2023-10-02 13:52:31 +0100

    fix: useless test

--- tests/method/test_pics.py
+++ tests/method/test_pics.py
@@ -2,16 +2,11 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 import pyspark.sql.functions as f
 from pyspark.sql import Row
 
 from otg.dataset.study_locus import StudyLocus
 from otg.method.pics import PICS
-
-if TYPE_CHECKING:
-    from pyspark.sql import SparkSession
 
 
 class TestFinemap:
@@ -42,51 +37,6 @@ class TestFinemap:
         observed_df = PICS.finemap(mock_study_locus).df.limit(1)
         print("TEST_FINEMAP_NULL", observed_df.show(truncate=False))
         assert observed_df.collect()[0]["locus"] is None
-
-    def test_finemap_null_r2(
-        self: TestFinemap,
-        spark: SparkSession,
-    ) -> None:
-        """Test finemap works when `r2Overall` is null by returning the same `locus` content."""
-        mock_study_locus_null_r2_data: list = [
-            (
-                1,
-                "varA",
-                "chr1",
-                10,
-                "studyA",
-                None,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                5.0,
-                -8,
-                0.0,
-                0.0,
-                "0",
-                [],
-                "pics",
-                [
-                    (
-                        "tagA",
-                        None,  # null r2
-                    )
-                ],
-                None,
-            )
-        ]
-
-        mock_study_locus = StudyLocus(
-            _df=spark.createDataFrame(
-                mock_study_locus_null_r2_data, schema=StudyLocus.get_schema()
-            ),
-            _schema=StudyLocus.get_schema(),
-        )
-        observed_df = PICS.finemap(mock_study_locus).df.limit(1)
-        # since PICS can't be run, it returns the same content
-        assert observed_df.collect()[0] == mock_study_locus.df.collect()[0]
 
 
 def test__finemap() -> None:
