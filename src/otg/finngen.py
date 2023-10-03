b93dc29f273d47a156fdfb280e895fe78c1d7ade
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-10-03 13:50:05 +0100

    feat: stub for running summary stats ingestion

--- src/otg/finngen.py
+++ src/otg/finngen.py
@@ -8,6 +8,7 @@ from urllib.request import urlopen
 from otg.common.session import Session
 from otg.config import FinnGenStepConfig
 from otg.datasource.finngen.study_index import FinnGenStudyIndex
+from otg.datasource.finngen.summary_stats import FinnGenSummaryStats
 
 
 @dataclass
@@ -31,7 +32,29 @@ class FinnGenStep(FinnGenStepConfig):
             self.finngen_sumstat_url_suffix,
         )
 
-        # Write the output.
+        # Write the study index output.
         finngen_studies.df.write.mode(self.session.write_mode).parquet(
             self.finngen_study_index_out
         )
+
+        # Ingest summary statistics
+        # This is a stub: final ingestion to be discussed further and implemented in a subsequent PR.
+        for row in finngen_studies.collect():
+            self.session.logger.info(
+                f"Processing {row.studyId} with summary statistics in {row.summarystatsLocation}"
+            )
+            summary_stats_df = (
+                self.session.spark.read.option("delimiter", "\t")
+                .csv(row.summarystatsLocation, header=True)
+                .repartition("#chrom")
+            )
+
+            # Process and output the data.
+            out_filename = f"{self.finngen_summary_stats_out}/{row.studyId}"
+            FinnGenSummaryStats.from_finngen_harmonized_summary_stats(
+                summary_stats_df, row.finngen_study_id
+            ).df.sortWithinPartitions("position").write.partitionBy("chromosome").mode(
+                self.session.write_mode
+            ).parquet(
+                out_filename
+            )
