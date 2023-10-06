385eaf1cfb6e873e67b4d400bb7f84fc924f8b9c
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-10-06 14:55:37 +0100

    test: update the tests

--- tests/datasource/finngen/test_finngen_summary_stats.py
+++ tests/datasource/finngen/test_finngen_summary_stats.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from pyspark.sql import DataFrame
 
 from otg.dataset.summary_statistics import SummaryStatistics
-from otg.preprocess.finngen.summary_stats import FinnGenSummaryStats
+from otg.datasource.finngen.summary_stats import FinnGenSummaryStats
 
 
 def test_finngen_summary_stats_from_source(
@@ -14,7 +14,7 @@ def test_finngen_summary_stats_from_source(
     """Test summary statistics from source."""
     assert isinstance(
         FinnGenSummaryStats.from_finngen_harmonized_summary_stats(
-            sample_finngen_summary_stats, "FINNGEN_R9_ACTINOMYCOSIS"
+            sample_finngen_summary_stats
         ),
         SummaryStatistics,
     )
