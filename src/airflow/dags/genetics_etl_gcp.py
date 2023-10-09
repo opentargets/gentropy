1162aab0d484f78a5b24580c5aeb77f841ada320
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-10-09 11:54:06 +0100

    refactor: submitting a PySpark job

--- src/airflow/dags/genetics_etl_gcp.py
+++ src/airflow/dags/genetics_etl_gcp.py
@@ -7,8 +7,7 @@ import pendulum
 import yaml
 from airflow.decorators import dag, task_group
 from airflow.operators.empty import EmptyOperator
-from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
-from airflow_common import generate_create_cluster_task, generate_delete_cluster_task
+from airflow_common import create_cluster, delete_cluster, submit_pyspark_job
 
 DAG_ID = "etl_using_external_flat_file"
 
@@ -24,7 +23,7 @@ otg_version = "0.1.4"
 initialisation_base_path = (
     f"gs://genetics_etl_python_playground/initialisation/{otg_version}"
 )
-python_cli = f"{initialisation_base_path}/cli.py"
+python_cli = "cli.py"
 config_name = "my_config"
 config_tar = f"{initialisation_base_path}/config.tar.gz"
 package_wheel = f"{initialisation_base_path}/otgenetics-{otg_version}-py3-none-any.whl"
@@ -46,50 +45,6 @@ default_args = {
 }
 
 
-def generate_pyspark_job_from_dict(
-    step: dict,
-    cluster_config_dir: str,
-    config_name: str,
-    cluster_name: str,
-) -> DataprocSubmitJobOperator:
-    """Generates a pyspark job from dictionary describing step.
-
-    Args:
-        step (dict): Dictionary describing step.
-        cluster_config_dir (str): Directory containing cluster config.
-        config_name (str): Name of config file.
-        cluster_name (str): Name of cluster.
-
-    Returns:
-        DataprocSubmitJobOperator: Operator for submitting pyspark job.
-    """
-    return DataprocSubmitJobOperator(
-        task_id=f"job-{step['id']}",
-        region="europe-west1",
-        project_id=project_id,
-        job={
-            "job_uuid": f"airflow-{step['id']}",
-            "reference": {"project_id": project_id},
-            "placement": {"cluster_name": cluster_name},
-            "pyspark_job": {
-                "main_python_file_uri": f"{initialisation_base_path}/cli.py",
-                "args": [
-                    f"step={ step['id'] }",
-                    f"--config-dir={ cluster_config_dir }",
-                    f"--config-name={ config_name }",
-                ],
-                "properties": {
-                    "spark.jars": "/opt/conda/miniconda3/lib/python3.10/site-packages/hail/backend/hail-all-spark.jar",
-                    "spark.driver.extraClassPath": "/opt/conda/miniconda3/lib/python3.10/site-packages/hail/backend/hail-all-spark.jar",
-                    "spark.executor.extraClassPath": "./hail-all-spark.jar",
-                    "spark.serializer": "org.apache.spark.serializer.KryoSerializer",
-                    "spark.kryo.registrator": "is.hail.kryo.HailKryoRegistrator",
-                },
-            },
-        },
-    )
-
-
 @dag(
     dag_id=Path(__file__).stem,
     default_args=default_args,
@@ -98,53 +53,51 @@ def generate_pyspark_job_from_dict(
 )
 def create_dag() -> None:
     """Submit dataproc workflow."""
+    assert (
+        SOURCE_CONFIG_FILE_PATH.exists()
+    ), f"Config path {SOURCE_CONFIG_FILE_PATH} does not exist."
+
     start = EmptyOperator(task_id="start")
-
-    source_config_file_path = Path(SOURCE_CONFIG_FILE_PATH)
-
     end = EmptyOperator(task_id="end", trigger_rule="all_done")
 
-    if source_config_file_path.exists():
-        with open(source_config_file_path, "r") as config_file:
-            tasks_groups = {}
-            steps = yaml.safe_load(config_file)
-            for step in steps:
-                print(step["id"])
+    with open(SOURCE_CONFIG_FILE_PATH, "r") as config_file:
+        tasks_groups = {}
+        steps = yaml.safe_load(config_file)
+        for step in steps:
+            step_id = step["id"]
 
-                @task_group(
-                    group_id=step["id"],
-                    prefix_group_id=True,
+            @task_group(
+                group_id=step_id,
+                prefix_group_id=True,
+            )
+            def tgroup(step_id: str) -> None:
+                """Self-sufficient task group for one task."""
+                cluster_name = f"workflow-otg-cluster-{step_id.replace('_', '-')}"
+                step_pyspark_job = submit_pyspark_job(
+                    cluster_name=cluster_name,
+                    task_id=f"job-{step_id}",
+                    python_module_path=python_cli,
+                    args=[
+                        f"step={step_id}",
+                        f"--config-dir={cluster_config_dir}",
+                        f"--config-name={config_name}",
+                    ],
                 )
-                def tgroup(step: dict) -> None:
-                    """Task group for step.
+                # Chain the steps within the task group.
+                (
+                    create_cluster(cluster_name)
+                    >> step_pyspark_job
+                    >> delete_cluster(cluster_name)
+                )
 
-                    Args:
-                        step (dict): Dictionary describing step.
-                    """
-                    cluster_name = (
-                        f"workflow-otg-cluster-{step['id'].replace('_', '-')}"
-                    )
-                    task = generate_pyspark_job_from_dict(
-                        step,
-                        cluster_config_dir=cluster_config_dir,
-                        config_name=config_name,
-                        cluster_name=cluster_name,
-                    )
-                    # Chain the steps within the task group.
-                    (
-                        generate_create_cluster_task(cluster_name)
-                        >> task
-                        >> generate_delete_cluster_task(cluster_name)
-                    )
+            thisgroup = tgroup(step_id)
+            tasks_groups[step_id] = thisgroup
+            if "prerequisites" in step:
+                for prerequisite in step["prerequisites"]:
+                    thisgroup.set_upstream(tasks_groups[prerequisite])
 
-                thisgroup = tgroup(step)
-                tasks_groups[step["id"]] = thisgroup
-                if "prerequisites" in step:
-                    for prerequisite in step["prerequisites"]:
-                        print(f"|- {prerequisite}")
-                        thisgroup.set_upstream(tasks_groups[prerequisite])
-
-                start >> thisgroup >> end
+            # Add task group to the DAG.
+            start >> thisgroup >> end
 
 
 dag = create_dag()
