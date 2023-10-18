d6258e862207f27c47d8cbdc5f8a15d611a09e2c
hlnicholls <53306752+hlnicholls@users.noreply.github.com>
2023-10-18 09:44:49 +0100

    fix: updated dentist func

--- src/otg/method/dentist.py
+++ src/otg/method/dentist.py
@@ -10,10 +10,11 @@ from otg.common.session import Session
 if TYPE_CHECKING:
     from pyspark.sql import DataFrame
 
-import numpy as np
+import math
+
 import pyspark.sql.functions as f
+from pyspark.sql.functions import udf
 from pyspark.sql.types import DoubleType
-from scipy import stats
 
 from otg.common.session import Session
 
@@ -38,25 +39,26 @@ class Dentist:
         """Performs outlier detection using DENTIST."""
         # need study locus summary statistics with columns: r2 with lead snp, beta, se, z
         # Calculate 'r'
-        filtered_StudyLocus = filtered_StudyLocus.withColumn(
-            "r", (f.sum("R2") * n_sample) / (f.count("R2") * n_sample)
+        # Calculate 'r' using aggregation
+        agg_result = filtered_StudyLocus.agg(
+            (f.sum("R2") * n_sample).alias("R2_sum"),
+            (f.count("R2") * n_sample).alias("R2_count"),
         )
+        agg_result = agg_result.withColumn(
+            "r", agg_result["R2_sum"] / agg_result["R2_count"]
+        )
+        filtered_StudyLocus = filtered_StudyLocus.crossJoin(agg_result.select("r"))
 
-        lead_idx_snp_row = filtered_StudyLocus.filter(
+        # Find the lead SNP
+        lead_idx_snp = filtered_StudyLocus.filter(
             filtered_StudyLocus.ID == lead_snp_ID
-        ).collect()[0]
-        lead_z = lead_idx_snp_row.beta / lead_idx_snp_row.se
+        ).first()
 
-        # 2. Calculate 't_dentist_s' and 'dentist_outlier'
+        # Calculate 't_dentist_s' and 'dentist_outlier'
+        lead_z = lead_idx_snp.Z
         filtered_StudyLocus = filtered_StudyLocus.withColumn(
             "t_dentist_s",
-            (
-                (
-                    filtered_StudyLocus.beta / filtered_StudyLocus.se
-                    - filtered_StudyLocus.r * lead_z
-                )
-                ** 2
-            )
+            ((filtered_StudyLocus.Z - filtered_StudyLocus.r * lead_z) ** 2)
             / (1 - filtered_StudyLocus.r**2),
         )
         filtered_StudyLocus = filtered_StudyLocus.withColumn(
@@ -67,19 +69,16 @@ class Dentist:
         )
 
         def calc_nlog10p_dentist_s(t_dentist_s):
-            return stats.chi2.logsf(t_dentist_s, df=1) / -np.log(10)
+            return math.log(1 - math.exp(-t_dentist_s)) / -math.log(10)
 
-        calc_nlog10p_dentist_s_udf = f.udf(calc_nlog10p_dentist_s, DoubleType())
+        udf_calc_nlog10p_dentist_s = F.udf(calc_nlog10p_dentist_s, DoubleType())
         filtered_StudyLocus = filtered_StudyLocus.withColumn(
-            "nlog10p_dentist_s", calc_nlog10p_dentist_s_udf("t_dentist_s")
+            "nlog10p_dentist_s",
+            udf_calc_nlog10p_dentist_s(filtered_StudyLocus["t_dentist_s"]),
         )
 
-        # Count the number of DENTIST outliers and creating new column
-        n_dentist_s_outlier = filtered_StudyLocus.filter(
-            (filtered_StudyLocus.R2 > r2_threshold)
-            & (filtered_StudyLocus.nlog10p_dentist_s > nlog10p_dentist_s_threshold)
-        ).count()
-        print(f"Number of DENTIST outliers detected: {n_dentist_s_outlier}")
+        # n_dentist_s_outlier = filtered_StudyLocus.filter((filtered_StudyLocus.R2 > r2_threshold) & (filtered_StudyLocus.nlog10p_dentist_s > nlog10p_dentist_s_threshold)).count()
+
         filtered_StudyLocus = filtered_StudyLocus.withColumn(
             "dentist_outlier",
             f.when(
@@ -88,4 +87,5 @@ class Dentist:
                 1,
             ).otherwise(0),
         )
+
         return filtered_StudyLocus
