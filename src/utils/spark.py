2db83fdf473aa5e980dbe8e7f798ee5367d001c8
Irene López <45119610+ireneisdoomed@users.noreply.github.com>
2023-10-25 10:07:59 +0100

    Update src/utils/spark.py
    Co-authored-by: Kirill Tsukanov <tskir@users.noreply.github.com>

--- src/utils/spark.py
+++ src/utils/spark.py
@@ -9,9 +9,9 @@ def get_spark_testing_conf() -> SparkConf:
     return (
         SparkConf()
         .set("spark.driver.bindAddress", "127.0.0.1")
-        # no shuffling
+        # No shuffling.
         .set("spark.sql.shuffle.partitions", "1")
-        # ui settings
+        # UI settings.
         .set("spark.ui.showConsoleProgress", "false")
         .set("spark.ui.enabled", "false")
         .set("spark.ui.dagGraph.retainedRootRDDs", "1")
@@ -21,6 +21,6 @@ def get_spark_testing_conf() -> SparkConf:
         .set("spark.sql.ui.retainedExecutions", "1")
         .set("spark.worker.ui.retainedExecutors", "1")
         .set("spark.worker.ui.retainedDrivers", "1")
-        # fixed memory
+        # Fixed memory.
         .set("spark.driver.memory", "2g")
     )
