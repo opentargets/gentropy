8643baa2bf591173d46d9aea3a8bf131fa18007b
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-10-26 10:05:06 +0100

    refactor: finalise LDIndexStepConfig

--- src/otg/config.py
+++ src/otg/config.py
@@ -33,41 +33,6 @@ class SessionConfig:
 
 
 @dataclass
-class LDIndexStepConfig:
-    """LD matrix step requirements.
-
-    Attributes:
-        ld_matrix_template (str): Template path for LD matrix from gnomAD.
-        ld_index_raw_template (str): Template path for the variant indices correspondance in the LD Matrix from gnomAD.
-        min_r2 (float): Minimum r2 to consider when considering variants within a window.
-        grch37_to_grch38_chain_path (str): Path to GRCh37 to GRCh38 chain file.
-        ld_populations (List[str]): List of population-specific LD matrices to process.
-        ld_index_out (str): Output LD index path.
-    """
-
-    _target_: str = "otg.ld_index.LDIndexStep"
-    ld_matrix_template: str = "gs://gcp-public-data--gnomad/release/2.1.1/ld/gnomad.genomes.r2.1.1.{POP}.common.adj.ld.bm"
-    ld_index_raw_template: str = "gs://gcp-public-data--gnomad/release/2.1.1/ld/gnomad.genomes.r2.1.1.{POP}.common.ld.variant_indices.ht"
-    min_r2: float = 0.5
-    grch37_to_grch38_chain_path: str = (
-        "gs://hail-common/references/grch37_to_grch38.over.chain.gz"
-    )
-    ld_populations: List[str] = field(
-        default_factory=lambda: [
-            "afr",  # African-American
-            "amr",  # American Admixed/Latino
-            "asj",  # Ashkenazi Jewish
-            "eas",  # East Asian
-            "fin",  # Finnish
-            "nfe",  # Non-Finnish European
-            "nwe",  # Northwestern European
-            "seu",  # Southeastern European
-        ]
-    )
-    ld_index_out: str = MISSING
-
-
-@dataclass
 class VariantIndexStepConfig:
     """Variant index step requirements.
 
