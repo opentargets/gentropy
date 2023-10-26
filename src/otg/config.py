ca4d86aaa535247144f650dfecdab12f890a94b6
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-10-26 10:06:44 +0100

    refactor: finalise StudyLocusOverlapStep

--- src/otg/config.py
+++ src/otg/config.py
@@ -126,22 +126,6 @@ class V2GStepConfig:
 
 
 @dataclass
-class StudyLocusOverlapStepConfig:
-    """StudyLocus overlaps index step requirements.
-
-    Attributes:
-        study_locus_path (str): Input study-locus path.
-        study_index_path (str): Input study index path to extract the type of study.
-        overlaps_index_out (str): Output overlaps index path.
-    """
-
-    _target_: str = "otg.overlaps.OverlapsIndexStep"
-    study_locus_path: str = MISSING
-    study_index_path: str = MISSING
-    overlaps_index_out: str = MISSING
-
-
-@dataclass
 class UKBiobankStepConfig:
     """UKBiobank study table ingestion step requirements.
 
