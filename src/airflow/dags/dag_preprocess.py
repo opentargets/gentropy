6dc37de8cea67b670bf31ef43daa789e7c7f390f
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-02 09:54:13 +0000

    refactor: use generate_dag() to simplify Preprocess DAG

--- src/airflow/dags/dag_preprocess.py
+++ src/airflow/dags/dag_preprocess.py
@@ -7,7 +7,7 @@ from airflow.models.dag import DAG
 
 from . import common_airflow as common
 
-CLUSTER_NAME = "workflow-otg-cluster"
+CLUSTER_NAME = "otg-preprocess"
 
 ALL_STEPS = [
     "finngen",
@@ -20,12 +20,8 @@ with DAG(
     default_args=common.shared_dag_args,
     **common.shared_dag_kwargs,
 ):
-    (
-        common.create_cluster(CLUSTER_NAME)
-        >> common.install_dependencies(CLUSTER_NAME)
-        >> [
-            common.submit_step(cluster_name=CLUSTER_NAME, step_id=step)
-            for step in ALL_STEPS
-        ]
-        >> common.delete_cluster(CLUSTER_NAME)
-    )
+    all_tasks = [
+        common.submit_step(cluster_name=CLUSTER_NAME, step_id=step)
+        for step in ALL_STEPS
+    ]
+    dag = common.generate_dag(cluster_name=CLUSTER_NAME, tasks=all_tasks)
