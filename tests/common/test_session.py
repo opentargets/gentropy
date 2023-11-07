59107185afdac4545dd9219a59972133c8766c33
Daniel Suveges <daniel.suveges@protonmail.com>
2023-11-07 20:57:50 +0000

    fix: un-comment rows in test_session.py

--- tests/common/test_session.py
+++ tests/common/test_session.py
@@ -3,15 +3,17 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from otg.common.session import Log4j, Session
+
 if TYPE_CHECKING:
-    pass
+    from pyspark.sql import SparkSession
 
 
-# def test_session_creation() -> None:
-#     """Test sessio creation with mock data."""
-#     assert isinstance(Session(spark_uri="local[1]"), Session)
+def test_session_creation() -> None:
+    """Test sessio creation with mock data."""
+    assert isinstance(Session(spark_uri="local[1]"), Session)
 
 
-# def test_log4j_creation(spark: SparkSession) -> None:
-#     """Test session log4j."""
-#     assert isinstance(Log4j(spark=spark), Log4j)
+def test_log4j_creation(spark: SparkSession) -> None:
+    """Test session log4j."""
+    assert isinstance(Log4j(spark=spark), Log4j)
