24e9be3b1ffff801a2ff317a9d33146cfce618e1
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-07 15:43:41 +0000

    feat: map partial and full study IDs

--- src/otg/eqtl_catalogue.py
+++ src/otg/eqtl_catalogue.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from urllib.request import urlopen
 
+import pyspark.sql.functions as f
 from omegaconf import MISSING
 
 from otg.common.session import Session
@@ -35,20 +36,43 @@ class EqtlStep:
         tsv_data = urlopen(self.eqtl_catalogue_paths_imported).read().decode("utf-8")
         rdd = self.session.spark.sparkContext.parallelize([tsv_data])
         df = self.session.spark.read.option("delimiter", "\t").csv(rdd)
-        # Process study index.
-        study_index = EqtlStudyIndex.from_source(df)
-        # Write study index.
-        study_index.df.write.mode(self.session.write_mode).parquet(
-            self.eqtl_catalogue_study_index_out
-        )
+        # Process partial study index.  At this point, it is not complete because we don't have the gene IDs, which we
+        # will only get once the summary stats are ingested.
+        study_index_df = EqtlStudyIndex.from_source(df).df
 
         # Fetch summary stats.
-        input_filenames = [row.summarystatsLocation for row in study_index.collect()]
+        input_filenames = [row.summarystatsLocation for row in study_index_df.collect()]
         summary_stats_df = self.session.spark.read.option("delimiter", "\t").csv(
             input_filenames, header=True
         )
         # Process summary stats.
         summary_stats_df = EqtlSummaryStats.from_source(summary_stats_df).df
+
+        # Explode study index based on the list of genes. While the original list contains one entry per tissue, what we
+        # consider as a single study is one mini-GWAS for an expression of a _particular gene_ in a particular study.
+        # At this stage we have a study index with partial study IDs like "PROJECT_QTLGROUP", and a summary statistics
+        # object with full study IDs like "PROJECT_QTLGROUP_GENEID", so we need to perform a merge and explosion to
+        # obtain our final study index.
+        partial_to_full_study_id = (
+            summary_stats_df.select(f.col("studyId"))
+            .distinct()
+            .select(
+                f.col("studyId").alias("fullStudyId"),  # PROJECT_QTLGROUP_GENEID
+                f.regexp_extract(f.col("studyId"), r"(.*)_[\_]+", 1).alias(
+                    "studyId"
+                ),  # PROJECT_QTLGROUP
+            )
+            .groupBy("studyId")
+            .agg(f.collect_list("fullStudyId").alias("fullStudyIdList"))
+        )
+        print(partial_to_full_study_id)
+
+        # f.regexp_extract(f.col("studyId"), r".*_([\_]+)", 1).alias("geneId"),  # GENEID
+
+        # Write study index.
+        study_index_df.write.mode(self.session.write_mode).parquet(
+            self.eqtl_catalogue_study_index_out
+        )
         # Write summary stats.
         (
             summary_stats_df.sortWithinPartitions("position")
