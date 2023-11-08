5987ac37489d1d70d0c4f6afd7deeb90f1296ccd
David Ochoa <dogcaesar@gmail.com>
2023-11-08 19:58:15 +0000

    test: rescue missing test

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -25,3 +25,11 @@ def mkdocs_build() -> None:
 def test_mkdocs_build() -> None:
     """Function to check if MkDocs build succeeded."""
     pass
+
+
+def test_check_missing_files(mkdocs_build: None) -> None:
+    """Test function to check for missing files in the documentation build."""
+    import os
+
+    build_dir = "site"
+    assert os.path.exists(build_dir), f"Build directory '{build_dir}' does not exist."
