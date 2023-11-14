871f5684a9738a4ec36ca5f0ff9e1f1ec84ef298
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-14 20:27:28 +0000

    chore: use more partitions for FinnGen

--- src/otg/finngen.py
+++ src/otg/finngen.py
@@ -54,8 +54,10 @@ class FinnGenStep:
 
         # Fetch summary stats.
         input_filenames = [row.summarystatsLocation for row in study_index.df.collect()]
-        summary_stats_df = self.session.spark.read.option("delimiter", "\t").csv(
-            input_filenames, header=True
+        summary_stats_df = (
+            self.session.spark.read.option("delimiter", "\t")
+            .csv(input_filenames, header=True)
+            .repartition(len(input_filenames) * 16)
         )
         # Process summary stats.
         summary_stats_df = FinnGenSummaryStats.from_source(summary_stats_df).df
