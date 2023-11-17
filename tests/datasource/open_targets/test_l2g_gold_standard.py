8347b2f896d5a808a57d924050cd7f498eb30a2a
Irene López <irene.lopezs@protonmail.com>
2023-11-17 15:09:10 +0000

    test: add `test_expand_gold_standard_with_negatives_same_positives`

--- tests/datasource/open_targets/test_l2g_gold_standard.py
+++ tests/datasource/open_targets/test_l2g_gold_standard.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
 from pyspark.sql import DataFrame
 
 from otg.dataset.l2g_gold_standard import L2GGoldStandard
@@ -36,34 +37,67 @@ def test_parse_positive_curation(
     assert df.columns == expected_cols, "GS parsing has a different schema."
 
 
-def test_expand_gold_standard_with_negatives(spark: SparkSession) -> None:
+class TestExpandGoldStandardWithNegatives:
     """Test expanding positive set with negative set."""
-    sample_positive_set = spark.createDataFrame(
-        [
-            ("variant1", "gene1", "study1"),
-            ("variant2", "gene2", "study1"),
-        ],
-        ["variantId", "geneId", "studyId"],
-    )
-    sample_v2g_df = spark.createDataFrame(
-        [
-            ("variant1", "gene1", 5, "X", "X", "X"),
-            ("variant1", "gene3", 10, "X", "X", "X"),
-        ],
-        ["variantId", "geneId", "distance", "chromosome", "datatypeId", "datasourceId"],
-    )
 
-    expected_expanded_gs = spark.createDataFrame(
-        [
-            ("variant1", "study1", "negative", "gene3"),
-            ("variant1", "study1", "positive", "gene1"),
-            ("variant2", "study1", "positive", "gene2"),
-        ],
-        ["variantId", "geneId", "goldStandardSet", "studyId"],
-    )
-    observed_df = OpenTargetsL2GGoldStandard.expand_gold_standard_with_negatives(
-        sample_positive_set, V2G(_df=sample_v2g_df, _schema=V2G.get_schema())
-    )
-    assert (
-        observed_df.collect() == expected_expanded_gs.collect()
-    ), "GS expansion is not as expected."
+    observed_df: DataFrame
+    expected_expanded_gs: DataFrame
+    sample_positive_set: DataFrame
+
+    def test_expand_gold_standard_with_negatives_logic(
+        self: TestExpandGoldStandardWithNegatives, spark: SparkSession
+    ) -> None:
+        """Test expanding positive set with negative set coincides with expected results."""
+        assert (
+            self.observed_df.collect() == self.expected_expanded_gs.collect()
+        ), "GS expansion is not as expected."
+
+    def test_expand_gold_standard_with_negatives_same_positives(
+        self: TestExpandGoldStandardWithNegatives, spark: SparkSession
+    ) -> None:
+        """Test expanding positive set with negative set doesn't remove any positives."""
+        assert (
+            self.observed_df.filter("goldStandardSet == 'positive'").count()
+            == self.sample_positive_set.count()
+        ), "GS expansion has removed positives."
+
+    @pytest.fixture(autouse=True)
+    def _setup(self: TestExpandGoldStandardWithNegatives, spark: SparkSession) -> None:
+        """Prepare fixtures for TestExpandGoldStandardWithNegatives."""
+        self.sample_positive_set = spark.createDataFrame(
+            [
+                ("variant1", "gene1", "study1"),
+                ("variant2", "gene2", "study1"),
+            ],
+            ["variantId", "geneId", "studyId"],
+        )
+
+        sample_v2g_df = spark.createDataFrame(
+            [
+                ("variant1", "gene1", 5, "X", "X", "X"),
+                ("variant1", "gene3", 10, "X", "X", "X"),
+            ],
+            [
+                "variantId",
+                "geneId",
+                "distance",
+                "chromosome",
+                "datatypeId",
+                "datasourceId",
+            ],
+        )
+
+        self.expected_expanded_gs = spark.createDataFrame(
+            [
+                ("variant1", "study1", "negative", "gene3"),
+                ("variant1", "study1", "positive", "gene1"),
+                ("variant2", "study1", "positive", "gene2"),
+            ],
+            ["variantId", "geneId", "goldStandardSet", "studyId"],
+        )
+        self.observed_df = (
+            OpenTargetsL2GGoldStandard.expand_gold_standard_with_negatives(
+                self.sample_positive_set,
+                V2G(_df=sample_v2g_df, _schema=V2G.get_schema()),
+            )
+        )
