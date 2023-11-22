8ca41a36c72dda2c4cc3a14a34d6ab95fc40f5dc
David Ochoa <dogcaesar@gmail.com>
2023-11-22 16:59:24 +0000

    feat: first version of finngen preprocessing dag

---
+++ src/airflow/dags/finngen_preprocess.py
@@ -0,0 +1,56 @@
+"""Airflow DAG for the Preprocess part of the pipeline."""
+from __future__ import annotations
+
+from pathlib import Path
+
+import common_airflow as common
+from airflow.models.dag import DAG
+from airflow.utils.trigger_rule import TriggerRule
+
+CLUSTER_NAME = "otg-preprocess-finngen"
+AUTOSCALING = "finngen-preprocess"
+
+with DAG(
+    dag_id=Path(__file__).stem,
+    description="Open Targets Genetics — Finngen preprocess",
+    default_args=common.shared_dag_args,
+    **common.shared_dag_kwargs,
+):
+    study_and_sumstats = common.submit_step(
+        cluster_name=CLUSTER_NAME,
+        step_id="finngen",
+        task_id="finngen_sumstats_and_study_index",
+    )
+
+    clumping = common.submit_step(
+        cluster_name=CLUSTER_NAME,
+        step_id="clump",
+        task_id="finngen_clump",
+        other_args=[
+            "step.summary_stats_path=gs://genetics_etl_python_playground/output/python_etl/parquet/XX.XX/summary_statistics/finngen",
+            "step.clumped_study_locus_out=gs://genetics_etl_python_playground/output/python_etl/parquet/XX.XX/study_locus/from_sumstats_study_locus/finngen",
+        ],
+        # This allows to attempt running the task when above step fails do to failifexists
+        trigger_rule=TriggerRule.ALL_DONE,
+    )
+
+    pics = common.submit_step(
+        cluster_name=CLUSTER_NAME,
+        step_id="pics",
+        task_id="finngen_pics",
+        other_args=[
+            "step.study_locus_ld_annotated_in=gs://genetics_etl_python_playground/output/python_etl/parquet/XX.XX/study_locus/from_sumstats_study_locus/finngen",
+            "step.picsed_study_locus_out=gs://genetics_etl_python_playground/output/python_etl/parquet/XX.XX/credible_set/from_sumstats_study_locus/finngen",
+        ],
+        # This allows to attempt running the task when above step fails do to failifexists
+        trigger_rule=TriggerRule.ALL_DONE,
+    )
+
+    (
+        common.create_cluster(CLUSTER_NAME, autoscaling_policy=AUTOSCALING)
+        >> common.install_dependencies(CLUSTER_NAME)
+        >> study_and_sumstats
+        >> clumping
+        >> pics
+        >> common.delete_cluster(CLUSTER_NAME)
+    )
