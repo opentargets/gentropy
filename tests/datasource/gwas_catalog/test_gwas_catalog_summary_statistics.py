70b8cb74d85de6f6e2273c7e6afd4c4547495150
Daniel Suveges <daniel.suveges@protonmail.com>
2023-11-24 23:45:04 +0000

    test: adding trst for old gwas format

--- tests/datasource/gwas_catalog/test_gwas_catalog_summary_statistics.py
+++ tests/datasource/gwas_catalog/test_gwas_catalog_summary_statistics.py
@@ -12,56 +12,101 @@ from otg.datasource.gwas_catalog.summary_statistics import GWASCatalogSummarySta
 
 if TYPE_CHECKING:
     from pyspark.sql import SparkSession
+    from pytest import FixtureRequest
 
 
-@pytest.fixture(scope="module")
-def gwas_catalog_summary_statistics__new_format(
-    spark: SparkSession,
-) -> GWASCatalogSummaryStatistics:
-    """Test GWASCatalogSummaryStatistics creation with mock data."""
-    return GWASCatalogSummaryStatistics.from_gwas_harmonized_summary_stats(
-        spark, "tests/data_samples/new_format_GCST90293086.h.tsv.gz"
+class TestGWASCatalogSummaryStatistics:
+    """Test suite for GWAS Catalog summary stats ingestion."""
+
+    @pytest.fixture(scope="class")
+    def gwas_catalog_summary_statistics__new_format(
+        self: TestGWASCatalogSummaryStatistics,
+        spark: SparkSession,
+    ) -> GWASCatalogSummaryStatistics:
+        """Test GWASCatalogSummaryStatistics creation with mock data."""
+        return GWASCatalogSummaryStatistics.from_gwas_harmonized_summary_stats(
+            spark, "tests/data_samples/new_format_GCST90293086.h.tsv.gz"
+        )
+
+    @pytest.fixture(scope="class")
+    def gwas_catalog_summary_statistics__old_format(
+        self: TestGWASCatalogSummaryStatistics,
+        spark: SparkSession,
+    ) -> GWASCatalogSummaryStatistics:
+        """Test GWASCatalogSummaryStatistics creation with mock data."""
+        return GWASCatalogSummaryStatistics.from_gwas_harmonized_summary_stats(
+            spark, "tests/data_samples/old_format_GCST006090.h.tsv.gz"
+        )
+
+    @pytest.fixture(scope="class")
+    def test_dataset_instance(
+        self: TestGWASCatalogSummaryStatistics, request: FixtureRequest
+    ) -> GWASCatalogSummaryStatistics:
+        """Meta fixture to return the value of any requested fixture."""
+        return request.getfixturevalue(request.param)
+
+    @pytest.mark.parametrize(
+        "test_dataset_instance",
+        [
+            "gwas_catalog_summary_statistics__old_format",
+            "gwas_catalog_summary_statistics__new_format",
+        ],
+        indirect=True,
     )
+    def test_return_type(
+        self: TestGWASCatalogSummaryStatistics,
+        test_dataset_instance: SummaryStatistics,
+    ) -> None:
+        """Testing return type."""
+        assert isinstance(test_dataset_instance, SummaryStatistics)
 
-
-def test_return_type(
-    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
-) -> None:
-    """Testing return type."""
-    assert isinstance(gwas_catalog_summary_statistics__new_format, SummaryStatistics)
-
-
-def test_p_value_parsed_correctly(
-    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
-) -> None:
-    """Testing parsed p-value."""
-    assert (
-        gwas_catalog_summary_statistics__new_format.df.filter(
-            f.col("pValueMantissa").isNotNull()
-        ).count()
-        > 1
+    @pytest.mark.parametrize(
+        "test_dataset_instance",
+        [
+            "gwas_catalog_summary_statistics__old_format",
+            "gwas_catalog_summary_statistics__new_format",
+        ],
+        indirect=True,
     )
+    def test_p_value_parsed_correctly(
+        self: TestGWASCatalogSummaryStatistics,
+        test_dataset_instance: SummaryStatistics,
+    ) -> None:
+        """Testing parsed p-value."""
+        assert (
+            test_dataset_instance.df.filter(f.col("pValueMantissa").isNotNull()).count()
+            > 1
+        )
 
-
-def test_effect_parsed_correctly(
-    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
-) -> None:
-    """Testing properly parsed effect."""
-    assert (
-        gwas_catalog_summary_statistics__new_format.df.filter(
-            f.col("beta").isNotNull()
-        ).count()
-        > 1
+    @pytest.mark.parametrize(
+        "test_dataset_instance",
+        [
+            "gwas_catalog_summary_statistics__old_format",
+            "gwas_catalog_summary_statistics__new_format",
+        ],
+        indirect=True,
     )
+    def test_effect_parsed_correctly(
+        self: TestGWASCatalogSummaryStatistics,
+        test_dataset_instance: SummaryStatistics,
+    ) -> None:
+        """Testing properly parsed effect."""
+        assert test_dataset_instance.df.filter(f.col("beta").isNotNull()).count() > 1
 
-
-def test_study_id(
-    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
-) -> None:
-    """Testing properly parsed effect."""
-    assert (
-        gwas_catalog_summary_statistics__new_format.df.filter(
-            f.col("studyId").startswith("GCST")
-        ).count()
-        == gwas_catalog_summary_statistics__new_format.df.count()
+    @pytest.mark.parametrize(
+        "test_dataset_instance",
+        [
+            "gwas_catalog_summary_statistics__old_format",
+            "gwas_catalog_summary_statistics__new_format",
+        ],
+        indirect=True,
     )
+    def test_study_id(
+        self: TestGWASCatalogSummaryStatistics,
+        test_dataset_instance: SummaryStatistics,
+    ) -> None:
+        """Testing properly parsed effect."""
+        assert (
+            test_dataset_instance.df.filter(f.col("studyId").startswith("GCST")).count()
+            == test_dataset_instance.df.count()
+        )
