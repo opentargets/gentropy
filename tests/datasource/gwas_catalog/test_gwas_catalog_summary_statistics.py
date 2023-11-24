c56cd6bae8f52227fffacc8097714bd05145dbbe
Daniel Suveges <daniel.suveges@protonmail.com>
2023-11-24 17:04:05 +0000

    feat: generalizing GWAS catalog sumstas ingestion both format supported

--- tests/datasource/gwas_catalog/test_gwas_catalog_summary_statistics.py
+++ tests/datasource/gwas_catalog/test_gwas_catalog_summary_statistics.py
@@ -4,19 +4,64 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pyspark.sql.functions as f
+import pytest
+
+from otg.dataset.summary_statistics import SummaryStatistics
 from otg.datasource.gwas_catalog.summary_statistics import GWASCatalogSummaryStatistics
 
 if TYPE_CHECKING:
-    from pyspark.sql import DataFrame
+    from pyspark.sql import SparkSession
 
 
-def test_gwas_catalog_summary_statistics_from_gwas_harmonized_summary_stats(
-    sample_gwas_catalog_harmonised_sumstats: DataFrame,
-) -> None:
+@pytest.fixture(scope="module")
+def gwas_catalog_summary_statistics__new_format(
+    spark: SparkSession,
+) -> GWASCatalogSummaryStatistics:
     """Test GWASCatalogSummaryStatistics creation with mock data."""
-    assert isinstance(
-        GWASCatalogSummaryStatistics.from_gwas_harmonized_summary_stats(
-            sample_gwas_catalog_harmonised_sumstats, "GCST000000"
-        ),
-        GWASCatalogSummaryStatistics,
+    return GWASCatalogSummaryStatistics.from_gwas_harmonized_summary_stats(
+        spark, "tests/data_samples/new_format_GCST90293086.h.tsv.gz"
+    )
+
+
+def test_return_type(
+    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
+) -> None:
+    """Testing return type."""
+    assert isinstance(gwas_catalog_summary_statistics__new_format, SummaryStatistics)
+
+
+def test_p_value_parsed_correctly(
+    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
+) -> None:
+    """Testing parsed p-value."""
+    assert (
+        gwas_catalog_summary_statistics__new_format.df.filter(
+            f.col("pValueMantissa").isNotNull()
+        ).count()
+        > 1
+    )
+
+
+def test_effect_parsed_correctly(
+    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
+) -> None:
+    """Testing properly parsed effect."""
+    assert (
+        gwas_catalog_summary_statistics__new_format.df.filter(
+            f.col("beta").isNotNull()
+        ).count()
+        > 1
+    )
+
+
+def test_study_id(
+    gwas_catalog_summary_statistics__new_format: SummaryStatistics,
+) -> None:
+    """Testing properly parsed effect."""
+    assert (
+        gwas_catalog_summary_statistics__new_format.df.filter(
+            f.col("studyId").startswith("GCST")
+        ).count()
+        == gwas_catalog_summary_statistics__new_format.df.count()
     )
