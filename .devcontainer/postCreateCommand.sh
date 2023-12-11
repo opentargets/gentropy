4ae2d590319342d3ff6d52ba0c9bef2109ee3e02
David Ochoa <dogcaesar@gmail.com>
2023-12-11 15:43:01 +0000

    feat: working version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Install Poetry environment
-poetry install --no-interaction --no-root --no-cache
-
-# Install Pre-commit hooks
-poetry run pre-commit install --install-hooks
+# # Install Pre-commit
+pre-commit install --install-hooks
