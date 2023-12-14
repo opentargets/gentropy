3534c8c82359b448c5337be0aa4c46fd6bd9c6cc
Daniel Suveges <daniel.suveges@protonmail.com>
2023-12-14 22:15:57 +0000

    test: improving test

--- tests/datasource/gwas_catalog/test_gwas_catalog_curation.py
+++ tests/datasource/gwas_catalog/test_gwas_catalog_curation.py
@@ -62,3 +62,50 @@ class TestGWASCatalogStudyCuration:
             "isCurated",
         ]
         return spark.createDataFrame(curation_data, curation_columns)
+
+    @staticmethod
+    def test_curation__return_type(
+        mock_gwas_study_index: StudyIndexGWASCatalog, mock_study_curation: DataFrame
+    ) -> None:
+        """Test return types."""
+        # Method should work on empty curation:
+        assert isinstance(
+            mock_gwas_study_index.annotate_from_study_curation(None),
+            StudyIndexGWASCatalog,
+        ), f"When applied None to curation function the returned type was: {type(mock_gwas_study_index.annotate_from_study_curation(None))}"
+
+        # Return type should work:
+        assert isinstance(
+            mock_gwas_study_index.annotate_from_study_curation(mock_study_curation),
+            StudyIndexGWASCatalog,
+        ), f"When applied None to curation function the returned type was: {type(mock_gwas_study_index.annotate_from_study_curation(mock_study_curation))}"
+
+    @staticmethod
+    def test_curation__returned_rows(
+        mock_gwas_study_index: StudyIndexGWASCatalog, mock_study_curation: DataFrame
+    ) -> None:
+        """Test return types."""
+        expected_count = mock_gwas_study_index.df.count()
+        zero_return_count = mock_gwas_study_index.annotate_from_study_curation(
+            None
+        ).df.count()
+        return_count = mock_gwas_study_index.annotate_from_study_curation(
+            mock_study_curation
+        ).df.count()
+        # Method should work on empty curation:
+        assert (
+            zero_return_count == expected_count
+        ), f"When applied None to curation function, the size of the returned data was not as expected ({zero_return_count} vs {expected_count})."
+
+        # Return type should work:
+        assert (
+            return_count == expected_count
+        ), f"When applied curation data, the size of the returned data was not as expected ({return_count} vs {expected_count})."
+
+    # Test updated type
+
+    # Test update qc flag
+
+    # Test updated method flag
+
+    # Test
