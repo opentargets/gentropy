4408fdf39d838db13066f68711df6a09d648deb4
Irene López <45119610+ireneisdoomed@users.noreply.github.com>
2023-12-19 15:57:08 +0000

    Update src/otg/dataset/study_locus.py
    Co-authored-by: David Ochoa <ochoa@ebi.ac.uk>

--- src/otg/dataset/study_locus.py
+++ src/otg/dataset/study_locus.py
@@ -50,7 +50,7 @@ class StudyLocusQualityCheck(Enum):
     UNRESOLVED_LD = "Variant not found in LD reference"
     LD_CLUMPED = "Explained by a more significant variant in high LD (clumped)"
     NO_POPULATION = "Study does not have population annotation to resolve LD"
-    UNPICSABLE = "Unable to calculate PIPs with the provided data"
+    NOT_QUALIFYING_LD_BLOCK = "LD block does not contain variants at the required R^2 threshold"
 
 
 class CredibleInterval(Enum):
