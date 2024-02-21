909c868a4d0769ffa8c76bbaa8936047ee92a9ae
David Ochoa <dogcaesar@gmail.com>
2024-02-21 15:16:15 +0000

    test: missing release prefix

--- tests/gentropy/datasource/finngen/test_finngen_finemapping.py
+++ tests/gentropy/datasource/finngen/test_finngen_finemapping.py
@@ -16,6 +16,7 @@ def test_finngen_finemapping_from_finngen_susie_finemapping(
             spark=spark,
             finngen_finemapping_df="tests/gentropy/data_samples/finngen_R9_AB1_EBV.SUSIE.snp.gz",
             finngen_finemapping_summaries="tests/gentropy/data_samples/finngen_credset_summary_sample.tsv",
+            finngen_release_prefix="FINNGEN_R10",
         ),
         StudyLocus,
     )
