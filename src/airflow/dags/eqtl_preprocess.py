01fa5d4857b8f6444f8267abdda329c12f03fa6e
Irene López <irene.lopezs@protonmail.com>
2024-02-29 16:14:29 +0000

    feat: run full dag

--- src/airflow/dags/eqtl_preprocess.py
+++ src/airflow/dags/eqtl_preprocess.py
@@ -6,14 +6,18 @@ from pathlib import Path
 
 import common_airflow as common
 from airflow.models.dag import DAG
+from airflow.providers.google.cloud.operators.dataflow import (
+    DataflowTemplatedJobStartOperator,
+)
+from airflow.providers.google.cloud.operators.gcs import GCSDeleteBucketOperator
 
 CLUSTER_NAME = "otg-preprocess-eqtl"
 AUTOSCALING = "do-ld-explosion"
 PROJECT_ID = "open-targets-genetics-dev"
 
 EQTL_CATALOG_SUSIE_LOCATION = "gs://eqtl_catalog_data/ebi_ftp/susie"
-TEMP_DECOMPRESS_LOCATION = "gs://eqtl_catalog_data/tmp_susie_decompressed"
-DECOMPRESS_FAILED_LOG = "gs://eqtl_catalog_data/tmp_susie_decompressed.log"
+TEMP_DECOMPRESS_LOCATION = "gs://eqtl_catalog_data/susie_decompressed_tmp"
+DECOMPRESS_FAILED_LOG = f"{TEMP_DECOMPRESS_LOCATION}.log"
 STUDY_INDEX_PATH = "gs://eqtl_catalog_data/study_index"
 CREDIBLE_SET_PATH = "gs://eqtl_catalog_data/credible_set_datasets/susie"
 
@@ -25,17 +29,17 @@ with DAG(
 ):
     # SuSIE fine mapping results are stored as gzipped files in a GCS bucket.
     # To improve processing performance, we decompress the files before processing to a temporary location in GCS.
-    # decompression_job = DataflowTemplatedJobStartOperator(
-    #     task_id="decompress_susie_outputs",
-    #     template="gs://dataflow-templates/latest/Bulk_Decompress_GCS_Files",
-    #     location="europe-west1",
-    #     project_id=PROJECT_ID,
-    #     parameters={
-    #         "inputFilePattern": f"{EQTL_CATALOG_SUSIE_LOCATION}/**/*.gz",
-    #         "outputDirectory": TEMP_DECOMPRESS_LOCATION,
-    #         "outputFailureFile": DECOMPRESS_FAILED_LOG,
-    #     },
-    # )
+    decompression_job = DataflowTemplatedJobStartOperator(
+        task_id="decompress_susie_outputs",
+        template="gs://dataflow-templates/latest/Bulk_Decompress_GCS_Files",
+        location="europe-west1",
+        project_id=PROJECT_ID,
+        parameters={
+            "inputFilePattern": f"{EQTL_CATALOG_SUSIE_LOCATION}/**/*.gz",
+            "outputDirectory": TEMP_DECOMPRESS_LOCATION,
+            "outputFailureFile": DECOMPRESS_FAILED_LOG,
+        },
+    )
 
     ingestion_job = common.submit_step(
         cluster_name=CLUSTER_NAME,
@@ -48,16 +52,16 @@ with DAG(
         ],
     )
 
-    # delete_decompressed_job = GCSDeleteBucketOperator(
-    #     task_id="delete_decompressed_files",
-    #     bucket_name=TEMP_DECOMPRESS_LOCATION,
-    #     force=True,
-    #     user_project=PROJECT_ID,
-    # )
+    delete_decompressed_job = GCSDeleteBucketOperator(
+        task_id="delete_decompressed_files",
+        bucket_name=TEMP_DECOMPRESS_LOCATION,
+        force=True,
+        user_project=PROJECT_ID,
+    )
 
     (
-        # decompression_job >>
-        common.create_cluster(
+        decompression_job
+        >> common.create_cluster(
             CLUSTER_NAME,
             autoscaling_policy=AUTOSCALING,
             num_workers=4,
@@ -65,6 +69,6 @@ with DAG(
         )
         >> common.install_dependencies(CLUSTER_NAME)
         >> ingestion_job
-        # >> delete_decompressed_job
-        # >> common.delete_cluster(CLUSTER_NAME)
+        >> delete_decompressed_job
+        >> common.delete_cluster(CLUSTER_NAME)
     )
