50d938e226b408114ebbcbefcc6387d0b0e77528
xyg123 <xiangyuge123@hotmail.co.uk>
2024-02-29 12:15:32 +0000

    fix: allow for coloc when only one snp is overlapping

--- tests/gentropy/method/test_colocalisation_method.py
+++ tests/gentropy/method/test_colocalisation_method.py
@@ -41,6 +41,51 @@ def test_coloc_colocalise(
         assert difference.filter(f.abs(f.col(col)) > threshold).count() == 0
 
 
+def test_single_snp_coloc(
+    spark: SparkSession,
+    threshold: float = 1e-5,
+) -> None:
+    """Test edge case of coloc where only one causal SNP is present in the StudyLocusOverlap."""
+    test_overlap_df = spark.createDataFrame(
+        [
+            {
+                "leftStudyLocusId": 1,
+                "rightStudyLocusId": 2,
+                "chromosome": "1",
+                "tagVariantId": "snp",
+                "left_logBF": 10.3,
+                "right_logBF": 10.5,
+            }
+        ]
+    )
+    test_overlap = StudyLocusOverlap(
+        test_overlap_df.select(
+            "leftStudyLocusId",
+            "rightStudyLocusId",
+            "chromosome",
+            "tagVariantId",
+            f.struct(f.col("left_logBF"), f.col("right_logBF")).alias("statistics"),
+        ),
+        StudyLocusOverlap.get_schema(),
+    )
+    test_result = Coloc.colocalise(test_overlap)
+
+    expected = spark.createDataFrame(
+        [
+            {
+                "h0": 9.254841951638903e-5,
+                "h1": 2.7517068829182966e-4,
+                "h2": 3.3609423764447284e-4,
+                "h3": 9.254841952564387e-13,
+                "h4": 0.9992961866536217,
+            }
+        ]
+    )
+    difference = test_result.df.select("h0", "h1", "h2", "h3", "h4").subtract(expected)
+    for col in difference.columns:
+        assert difference.filter(f.abs(f.col(col)) > threshold).count() == 0
+
+
 def test_ecaviar(mock_study_locus_overlap: StudyLocusOverlap) -> None:
     """Test eCAVIAR."""
     assert isinstance(ECaviar.colocalise(mock_study_locus_overlap), Colocalisation)
