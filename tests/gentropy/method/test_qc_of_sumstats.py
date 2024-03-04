778eaa6519c1d2eb2d2b34df04f1f22f45529fe6
Yakov Tsepilov <yt4@sanger.ac.uk>
2024-03-04 16:19:33 +0000

    fix: fixing tests and adding rounding

--- tests/gentropy/method/test_qc_of_sumstats.py
+++ tests/gentropy/method/test_qc_of_sumstats.py
@@ -1,6 +1,7 @@
 """Test of the qc of summary statistics."""
 from __future__ import annotations
 
+import numpy as np
 import pyspark.sql.functions as f
 from gentropy.dataset.summary_statistics import SummaryStatistics
 
@@ -12,10 +13,10 @@ def test_qc_functions(
     gwas = sample_summary_statistics.sanity_filter()
 
     assert gwas.number_of_snps() == (1663, 29)
-    assert gwas.gc_lambda_check(lambda_threshold=2) == (True, 1.9159734429793385)
-    assert gwas.sumstat_qc_beta_check() == (True, 0.001310962803607386)
+    assert np.round(gwas.gc_lambda_check(lambda_threshold=2)[1], 4) == 1.916
+    assert np.round(gwas.sumstat_qc_beta_check()[1], 4) == 0.0013
     assert gwas.sumstat_n_eff_check(n_total=100000) == (True, 0)
-    assert gwas.sumstat_qc_pz_check() == (True, 1.0, -1.0014145068737434e-10)
+    assert np.sum(np.round(gwas.sumstat_qc_pz_check(), 6)) == 2
 
 
 def test_neff_check_eaf(
@@ -27,4 +28,4 @@ def test_neff_check_eaf(
     gwas_df = gwas_df.withColumn("effectAlleleFrequencyFromSource", f.lit(0.5))
     gwas._df = gwas_df
 
-    assert gwas.sumstat_n_eff_check(n_total=100000) == (True, 0.5585536469698384)
+    assert np.round(gwas.sumstat_n_eff_check(n_total=100000)[1], 4) == 0.5586
