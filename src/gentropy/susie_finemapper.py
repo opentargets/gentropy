a4e4bdfdb1c1dc25c332c14183cbceafa5c1721c
Daniel Considine <dc16@sanger.ac.uk>
2024-04-23 11:01:22 +0100

    feat: adding init to finemapping step

--- src/gentropy/susie_finemapper.py
+++ src/gentropy/susie_finemapper.py
@@ -28,6 +28,45 @@ class SusieFineMapperStep:
     In the future this step will be refactored and moved to the methods module.
     """
 
+    def __init__(
+        self,
+        session: Session,
+        study_locus_to_finemap: str,
+        study_locus_collected_path: str,
+        study_index_path: str,
+        output_path: str,
+        locus_radius: int = 500_000,
+        locus_L: int = 10,
+    ) -> None:
+        """Run fine-mapping on a studyLocusId from a collected studyLocus table.
+
+        Args:
+            session (Session): Spark session
+            study_locus_to_finemap (str): path to the study locus to fine-map
+            study_locus_collected_path (str): path to the collected study locus
+            study_index_path (str): path to the study index
+            output_path (str): path to the output
+            locus_radius (int): Radius of base-pair window around the locus, default is 500_000
+            locus_L (int): Maximum number of causal variants in locus, default is 10
+        """
+        # Read studyLocus
+        study_locus = (
+            StudyLocus.from_parquet(session, study_locus_collected_path)
+            .df.filter(f.col("studyLocusId") == study_locus_to_finemap)
+            .collect()[0]
+        )
+        study_index = StudyIndex.from_parquet(session, study_index_path)
+        # Run fine-mapping
+        result = self.susie_finemapper_ss_gathered(
+            session,
+            study_locus,
+            study_index,
+            locus_radius * 2,
+            locus_L,
+        )
+        # Write result
+        result.df.write.mode(session.write_mode).parquet(output_path)
+
     @staticmethod
     def susie_finemapper_one_studylocus_row(
         GWAS: SummaryStatistics,
@@ -82,9 +121,9 @@ class SusieFineMapperStep:
             .filter(f.col("z").isNotNull())
         )
         # Remove ALL duplicated variants from GWAS DataFrame - we don't know which is correct
-        variant_counts = gwas_df.groupBy("variantId").count()
-        unique_variants = variant_counts.filter(f.col("count") == 1)
-        gwas_df = gwas_df.join(unique_variants, on="variantId", how="left_semi")
+        # variant_counts = gwas_df.groupBy("variantId").count()
+        # unique_variants = variant_counts.filter(f.col("count") == 1)
+        # gwas_df = gwas_df.join(unique_variants, on="variantId", how="left_semi")
 
         ld_index = (
             GnomADLDMatrix()
