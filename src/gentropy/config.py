bc1a1124b0744895f2ce246c23b62861ea600288
Irene López <45119610+ireneisdoomed@users.noreply.github.com>
2024-05-15 09:20:58 +0100

    feat(airflow): include COLOC as a node in the DAG (#530)
    * feat: split colocalisation step into two nodes: coloc and ecaviar
    
    * feat: filter credible sets to be colocalised based on method
    
    * fix(dag): update coloc dependencies
    
    * revert(config): revert colocalisation output location
    
    * feat(coloc): _get_colocalisation_class to import methods dynamically
    
    * refactor(step): remove method enum
    
    * refactor(step): do not hardcode method value
    
    * fix(config): typo in static_assets
    
    * refactor(dag): uncomment steps

--- src/gentropy/config.py
+++ src/gentropy/config.py
@@ -37,6 +37,7 @@ class ColocalisationConfig(StepConfig):
     credible_set_path: str = MISSING
     study_index_path: str = MISSING
     coloc_path: str = MISSING
+    colocalisation_method: str = MISSING
     _target_: str = "gentropy.colocalisation.ColocalisationStep"
 
 
