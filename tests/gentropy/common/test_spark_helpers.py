7f2835be0ce2b55c509b258253e4fc434a391eac
DSuveges <daniel.suveges@protonmail.com>
2024-05-20 21:53:42 +0100

    test: adding test for array sorter

--- tests/gentropy/common/test_spark_helpers.py
+++ tests/gentropy/common/test_spark_helpers.py
@@ -3,10 +3,54 @@
 from __future__ import annotations
 
 import pytest
-from gentropy.common.spark_helpers import enforce_schema
 from pyspark.sql import Column, SparkSession
 from pyspark.sql import functions as f
 from pyspark.sql import types as t
+
+from gentropy.common.spark_helpers import (
+    enforce_schema,
+    order_array_of_structs_by_field,
+)
+
+
+def test__order_array_of_structs_by_field(spark: SparkSession) -> None:
+    """Test order_array_of_structs_by_field."""
+    data = [
+        # Values are the same:
+        ("a", 12),
+        ("a", 12),
+        # First value bigger:
+        ("b", 12),
+        ("b", 1),
+        # Second value bigger:
+        ("c", 1),
+        ("c", 12),
+        # First value is null:
+        ("d", None),
+        ("d", 12),
+        # Second value is null:
+        ("e", 12),
+        ("e", None),
+        # Both values are null:
+        ("f", None),
+        ("f", None),
+    ]
+
+    processed_data = (
+        spark.createDataFrame(data, ["group", "value"])
+        .groupBy("group")
+        .agg(
+            f.collect_list(f.struct(f.col("value").alias("value"))).alias("values"),
+            f.max(f.col("value")).alias("max_value"),
+        )
+        .withColumn("sorted_values", order_array_of_structs_by_field("values", "value"))
+        .withColumn("sorted_max", f.col("sorted_values")[0].getField("value"))
+        .select("max_value", "sorted_max")
+        .collect()
+    )
+
+    for row in processed_data:
+        assert row["max_value"] == row["sorted_max"]
 
 
 class TestEnforceSchema:
@@ -20,8 +64,8 @@ class TestEnforceSchema:
         ]
     )
 
-    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     @staticmethod
+    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     def test_function_good_schema() -> Column:
         """Create a struct with the expected schema."""
         return f.struct(
@@ -30,8 +74,8 @@ class TestEnforceSchema:
             f.lit("test3").alias("field3"),
         )
 
-    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     @staticmethod
+    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     def test_function_missing_column() -> Column:
         """Create a struct with a missing column."""
         return f.struct(
@@ -39,8 +83,8 @@ class TestEnforceSchema:
             f.lit("test3").alias("field3"),
         )
 
-    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     @staticmethod
+    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     def test_function_wrong_order() -> Column:
         """Create a struct with the wrong order."""
         return f.struct(
@@ -48,8 +92,8 @@ class TestEnforceSchema:
             f.lit("test1").alias("field1"),
         )
 
-    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     @staticmethod
+    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     def test_function_extra_column() -> Column:
         """Create a struct with an extra column."""
         return f.struct(
@@ -58,8 +102,8 @@ class TestEnforceSchema:
             f.lit("test4").alias("field4"),
         )
 
-    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     @staticmethod
+    @enforce_schema(expected_schema=EXPECTED_SCHEMA)
     def test_function_wrong_type() -> Column:
         """Create a struct with the wrong type."""
         return f.struct(
@@ -87,7 +131,6 @@ class TestEnforceSchema:
         """Test enforce schema consistency."""
         # Looping through all the struct column and test if the schema is consistent
         for column in ["struct_1", "struct_2", "struct_3", "struct_4", "struct_5"]:
-            assert (
-                self.test_dataset.schema[column].dataType.dataType
-                == self.EXPECTED_SCHEMA
-            )
+            assert self.test_dataset.schema[column].dataType == self.EXPECTED_SCHEMA
+        for column in ["struct_1", "struct_2", "struct_3", "struct_4", "struct_5"]:
+            assert self.test_dataset.schema[column].dataType == self.EXPECTED_SCHEMA
