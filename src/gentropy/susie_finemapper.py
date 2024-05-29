07c9096c33108aa0a6d067d353015508be23bf58
Kirill Tsukanov <tskir@users.noreply.github.com>
2024-05-29 13:02:51 +0100

    fix(susie_finemapper): use parquet instead of csv

--- src/gentropy/susie_finemapper.py
+++ src/gentropy/susie_finemapper.py
@@ -125,8 +125,9 @@ class SusieFineMapperStep:
                 output_path + "/" + study_locus_to_finemap
             )
             # Write log
-            result_logging["log"].to_csv(
-                output_path_log + "/" + study_locus_to_finemap + ".csv",
+            result_logging["log"].to_parquet(
+                output_path_log + "/" + study_locus_to_finemap + ".parquet",
+                engine="pyarrow",
                 index=False
             )
         else:
