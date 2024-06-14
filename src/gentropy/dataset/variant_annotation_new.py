bbb18af03bd70be85cbb374cf10f8bf8b3f1c454
DSuveges <daniel.suveges@protonmail.com>
2024-06-14 11:12:33 +0100

    refactor(variant annotation): migrating methods to the new schema

--- src/gentropy/dataset/variant_annotation_new.py
+++ src/gentropy/dataset/variant_annotation_new.py
@@ -17,9 +17,12 @@ from gentropy.common.schemas import parse_spark_schema
 from gentropy.common.session import Session
 from gentropy.common.spark_helpers import (
     enforce_schema,
-    order_array_of_structs_by_field,
+    order_array_of_structs_by_field, 
+    normalise_column
 )
 from gentropy.dataset.dataset import Dataset
+from gentropy.dataset.gene_index import GeneIndex
+from gentropy.dataset.v2g import V2G
 
 if TYPE_CHECKING:
     from pyspark.sql import Column, DataFrame
@@ -547,4 +550,172 @@ class VariantAnnotationNew(Dataset):
         return cls(
             _df=VEP_parser.process_vep_output(vep_data),
             _schema=variant_annotation_schema,
+        )
+
+    def add_annotation(self: Self, annotation: DataFrame) -> Self:
+        """Add annotation to the dataset.
+
+        At this point the variant annotation can be extended with extra cross-references and allele frequencies.
+
+        Args:
+            annotation (DataFrame): Annotation to add to the dataset
+
+        Returns:
+            Self: Dataset with the annotation added
+        """
+        # Assert that the provided annotation has unique variant identifiers:
+        assert (
+            annotation.select("variantId").distinct().count() == annotation.count()
+        ), "Annotation must have unique variant identifiers."
+
+        # Rename columns to avoid conflicts:
+        renamed_columns = {
+            col: f"annotation_{col}" for col in annotation.columns if col != "variantId"
+        }
+        annotation = annotation.select(
+            *[f.col(col).alias(renamed_columns[col]) for col in annotation.columns]
+        )
+        # Join the annotation to the dataset:
+        jonied = self.df.join(annotation, on="variantId", how="left")
+
+        # Merge cross-ref if present:
+        if "dbXrefs" in renamed_columns:
+            jonied = jonied.withColumn(
+                "dbXrefs",
+                f.array_union(
+                    f.col("dbXrefs"),
+                    f.col("annotation_dbXrefs"),
+                ),
+            )
+
+        # Rename population column if not present in the dataset:
+        if ("alleleFrequencies" in renamed_columns) and not in jonied.columns:
+            jonied = jonied.withColumnRenamed(
+                "annotation_alleleFrequencies",
+                "alleleFrequencies",
+            )
+
+        # Drop the annotation columns:
+        jonied = jonied.drop(*[col for col in jonied.columns if "annotation_" in col])
+
+        # Join the annotation to the dataset:
+        return VariantAnnotationNew(
+            _df=jonied,
+            _schema=self.schema,
+        )
+
+    def max_maf(self: VariantAnnotationNew) -> Column:
+        """Maximum minor allele frequency accross all populations.
+
+        Returns:
+            Column: Maximum minor allele frequency accross all populations.
+
+        Raises:
+            ValueError: Allele frequencies are not present in the dataset.
+        """
+        if "alleleFrequencies" not in self.df.columns:
+            raise ValueError("Allele frequencies are not present in the dataset.")
+
+        return f.array_max(
+            f.transform(
+                self.df.alleleFrequencies,
+                lambda af: f.when(
+                    af.alleleFrequency > 0.5, 1 - af.alleleFrequency
+                ).otherwise(af.alleleFrequency),
+            )
+        )
+
+    def filter_by_variant_df(
+        self: VariantAnnotationNew, df: DataFrame
+    ) -> VariantAnnotationNew:
+        """Filter variant annotation dataset by a variant dataframe.
+
+        Args:
+            df (DataFrame): A dataframe of variants
+
+        Returns:
+            VariantAnnotationNew: A filtered variant annotation dataset
+        """
+        self.df = self._df.join(
+            f.broadcast(df.select("variantId", "chromosome")),
+            on=["variantId", "chromosome"],
+            how="inner",
+        )
+        return self
+
+    def get_transcript_consequence_df(
+        self: VariantAnnotationNew, gene_index: GeneIndex | None = None
+    ) -> DataFrame:
+        """Dataframe of exploded transcript consequences.
+
+        Optionally the trancript consequences can be reduced to the universe of a gene index.
+
+        Args:
+            gene_index (GeneIndex | None): A gene index. Defaults to None.
+
+        Returns:
+            DataFrame: A dataframe exploded by transcript consequences with the columns variantId, chromosome, transcriptConsequence
+        """
+        # exploding the array removes records without VEP annotation
+        transript_consequences = self.df.withColumn(
+            "transcriptConsequence", f.explode("transcriptConsequences")
+        ).select(
+            "variantId",
+            "chromosome",
+            "position",
+            "transcriptConsequence",
+            f.col("transcriptConsequence.geneId").alias("geneId"),
+        )
+        if gene_index:
+            transript_consequences = transript_consequences.join(
+                f.broadcast(gene_index.df),
+                on=["chromosome", "geneId"],
+            )
+        return transript_consequences.persist()
+
+    def get_distance_to_tss(
+        self: VariantAnnotationNew,
+        gene_index: GeneIndex,
+        max_distance: int = 500_000,
+    ) -> V2G:
+        """Extracts variant to gene assignments for variants falling within a window of a gene's TSS.
+
+        Args:
+            gene_index (GeneIndex): A gene index to filter by.
+            max_distance (int): The maximum distance from the TSS to consider. Defaults to 500_000.
+
+        Returns:
+            V2G: variant to gene assignments with their distance to the TSS
+        """
+        return V2G(
+            _df=(
+                self.df.alias("variant")
+                .join(
+                    f.broadcast(gene_index.locations_lut()).alias("gene"),
+                    on=[
+                        f.col("variant.chromosome") == f.col("gene.chromosome"),
+                        f.abs(f.col("variant.position") - f.col("gene.tss"))
+                        <= max_distance,
+                    ],
+                    how="inner",
+                )
+                .withColumn(
+                    "distance", f.abs(f.col("variant.position") - f.col("gene.tss"))
+                )
+                .withColumn(
+                    "inverse_distance",
+                    max_distance - f.col("distance"),
+                )
+                .transform(lambda df: normalise_column(df, "inverse_distance", "score"))
+                .select(
+                    "variantId",
+                    f.col("variant.chromosome").alias("chromosome"),
+                    "distance",
+                    "geneId",
+                    "score",
+                    f.lit("distance").alias("datatypeId"),
+                    f.lit("canonical_tss").alias("datasourceId"),
+                )
+            ),
+            _schema=V2G.get_schema(),
         )
