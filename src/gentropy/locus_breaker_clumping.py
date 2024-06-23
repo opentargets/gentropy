2e314e191cf8ed8de29bfe5ac937500a924dfb24
Daniel Considine <dc16@sanger.ac.uk>
2024-06-23 14:31:51 +0100

    feat: locus breaker clumping step

--- src/gentropy/locus_breaker_clumping.py
+++ src/gentropy/locus_breaker_clumping.py
@@ -3,32 +3,62 @@
 from __future__ import annotations
 
 from gentropy.common.session import Session
-from gentropy.dataset.study_locus import StudyLocus
+from gentropy.dataset.summary_statistics import SummaryStatistics
+from gentropy.method.locus_breaker_clumping import LocusBreakerClumping
 
 
 class LocusBreakerClumpingStep:
-    """Step to perform locus-breaker clumping on study locus dataset."""
+    """Step to perform locus-breaker clumping on a study."""
 
     def __init__(
         self,
         session: Session,
-        study_locus_input_path: str,
-        study_index_path: str,
+        summary_statistics_input_path: str,
         clumped_study_locus_output_path: str,
+        lbc_baseline_pvalue: float,
+        lbc_distance_cutoff: int,
+        lbc_pvalue_threshold: float,
+        lbc_flanking_distance: int,
+        large_loci_size: int,
+        wbc_pvalue_threshold: float,
+        collect_locus_distance: int,
+        collect_locus: bool = False,
     ) -> None:
         """Run locus-breaker clumping step.
 
         Args:
             session (Session): Session object.
-            study_locus_input_path (str): Path to the input study locus.
-            study_index_path (str): Path to the study index.
+            summary_statistics_input_path (str): Path to the input study locus.
             clumped_study_locus_output_path (str): path of the resulting, clumped study-locus dataset.
+            lbc_baseline_pvalue (float): Baseline p-value for locus breaker clumping.
+            lbc_distance_cutoff (int): Distance cutoff for locus breaker clumping.
+            lbc_pvalue_threshold (float): P-value threshold for locus breaker clumping.
+            lbc_flanking_distance (int): Flanking distance for locus breaker clumping.
+            large_loci_size (int): Size of large loci.
+            wbc_pvalue_threshold (float): P-value threshold for window breaker clumping.
+            collect_locus_distance (int): Distance to collect locus.
+            collect_locus (bool, optional): Whether to collect locus. Defaults to False.
         """
-        study_locus = StudyLocus.from_parquet(session, study_locus_input_path)
+        sum_stats = SummaryStatistics.from_parquet(
+            session, summary_statistics_input_path
+        )
+        lbc = sum_stats.locus_breaker_clumping(
+            lbc_baseline_pvalue,
+            lbc_distance_cutoff,
+            lbc_pvalue_threshold,
+            lbc_flanking_distance,
+        )
+        clumped_result = LocusBreakerClumping._process_locus_breaker(
+            lbc,
+            sum_stats,
+            large_loci_size,
+            wbc_pvalue_threshold,
+        )
+        if collect_locus:
+            clumped_result = clumped_result.annotate_locus_statistics(
+                sum_stats, collect_locus_distance=collect_locus_distance
+            )
 
-        (
-            study_locus.clump()
-            # Save result:
-            .df.write.mode(session.write_mode)
-            .parquet(clumped_study_locus_output_path)
+        clumped_result.df.write.mode(session.write_mode).parquet(
+            clumped_study_locus_output_path
         )
