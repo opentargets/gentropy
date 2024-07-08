5f972320272be87bebc6c43cf07cce248bfb0f81
Daniel-Considine <113430683+Daniel-Considine@users.noreply.github.com>
2024-07-08 13:21:05 +0100

    feat: locus_breaker_clumping (#655)
    * feat: locus_breaker_clumping
    
    * fix: dosctring
    
    * feat: _process_locus_breaker function
    
    * feat: locus breaker clumping step
    
    * fix: tidying parameters
    
    * feat: option to remove MHC region
    
    * fix: description for LocusBreakerClumpingStep
    
    * fix: removing division of distance
    
    * fix: adding new parameters for wbc distance separate from large_loci_size
    
    * fix: resolving comments
    
    * refactor: refactored code in process_locus_breaker_output
    
    * fix: removing superfluous variable
    
    * fix: persisting sumstats parquet to improve analysis plan
    
    ---------
    
    Co-authored-by: Yakov <yt4@sanger.ac.uk>

--- src/gentropy/config.py
+++ src/gentropy/config.py
@@ -382,7 +382,7 @@ class LocusBreakerClumpingConfig(StepConfig):
         }
     )
     distance_cutoff: int = 250_000
-    flankig_distance: int = 100_000
+    flanking_distance: int = 100_000
     baseline_pvalue_cutoff: float = 1e-5
 
 
