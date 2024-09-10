6f0a2e2711c367b5331d7cb52ac63c7118f8259b
Tobi Alegbe <alegbe@ebi.ac.uk>
2024-09-10 09:42:39 +0100

    Make early file for merging multiple biosample indices into one

---
+++ src/gentropy/biosample_index.py
@@ -0,0 +1,37 @@
+"""Step to generate biosample index dataset."""
+from __future__ import annotations
+
+from gentropy.common.session import Session
+from gentropy.datasource.open_targets.target import OpenTargetsTarget
+from gentropy.dataset.biosample_index import BiosampleIndex
+from gentropy.datasource.cell_ontology.biosample_index import CellOntologyBiosampleIndex
+from gentropy.datasource.uberon.biosample_index import UberonBiosampleIndex
+
+
+class BiosampleIndexStep:
+    """Biosample index step.
+
+    This step generates a Biosample index dataset from the various ontology sources. Currently Cell Ontology and Uberon are supported.
+    """
+
+    def __init__(
+        self,
+        session: Session,
+        cell_ontology_input_path: str,
+        uberon_input_path: str,
+        biosample_index_output_path: str,
+    ) -> None:
+        """Run Biosample index generation step.
+
+        Args:
+            session (Session): Session object.
+            cell_ontology_input_path (str): Input cell ontology dataset path.
+            uberon_input_path (str): Input uberon dataset path.
+            biosample_index_output_path (str): Output gene index dataset path.
+        """
+        cell_ontology_index = CellOntologyBiosampleIndex.extract_celltypes_from_source(
+            session, cell_ontology_input_path
+        )
+        uberon_index = UberonBiosampleIndex.extract_tissue_from_source(
+            session, uberon_input_path
+        )
