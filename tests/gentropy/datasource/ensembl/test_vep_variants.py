6c6918fa1a3a775c930cca8a9ec485805b9a637e
DSuveges <daniel.suveges@protonmail.com>
2024-09-27 11:20:37 +0100

    test: removing skip as the vep parser logic is fixed

--- tests/gentropy/datasource/ensembl/test_vep_variants.py
+++ tests/gentropy/datasource/ensembl/test_vep_variants.py
@@ -68,7 +68,6 @@ class TestVEPParserInSilicoExtractor:
 
         self.df = parsed_df
 
-    @pytest.mark.skip(reason="Not implemented.")
     def test_in_silico_output_missing_value(
         self: TestVEPParserInSilicoExtractor,
     ) -> None:
@@ -109,7 +108,6 @@ class TestVEPParser:
             self.raw_vep_output, 200
         )
 
-    @pytest.mark.skip(reason="Not implemented.")
     def test_extract_variant_index_from_vep(
         self: TestVEPParser, spark: SparkSession
     ) -> None:
@@ -137,14 +135,12 @@ class TestVEPParser:
             == in_silico_schema
         ), "In silico schema is not correct."
 
-    @pytest.mark.skip(reason="Not implemented.")
     def test_process(self: TestVEPParser) -> None:
         """Test process method."""
         df = VariantEffectPredictorParser.process_vep_output(self.raw_vep_output)
         assert isinstance(df, DataFrame), "Processed VEP output is not a DataFrame."
         assert df.count() > 0, "No variant data in processed VEP dataframe."
 
-    @pytest.mark.skip(reason="Not implemented.")
     def test_conversion(self: TestVEPParser) -> None:
         """Test if processed data can be converted into a VariantIndex object."""
         variant_index = VariantIndex(
@@ -156,7 +152,6 @@ class TestVEPParser:
             variant_index, VariantIndex
         ), "VariantIndex object not created."
 
-    @pytest.mark.skip(reason="Not implemented.")
     def test_variant_count(self: TestVEPParser) -> None:
         """Test if the number of variants is correct.
 
