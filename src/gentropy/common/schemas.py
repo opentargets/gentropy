31e2da200494ecc4e2cb676e4c875f9671c40064
DSuveges <daniel.suveges@protonmail.com>
2024-09-30 12:26:12 +0100

    fix(schemas): removing schema flattening function

--- src/gentropy/common/schemas.py
+++ src/gentropy/common/schemas.py
@@ -55,44 +55,6 @@ def parse_spark_schema(schema_json: str) -> StructType:
     return StructType.fromJson(core_schema)
 
 
-def flatten_schema(schema: StructType, prefix: str = "") -> list[Any]:
-    """It takes a Spark schema and returns a list of all fields in the schema once flattened.
-
-    Args:
-        schema (StructType): The schema of the dataframe
-        prefix (str): The prefix to prepend to the field names. Defaults to "".
-
-    Returns:
-        list[Any]: A list of all the columns in the dataframe.
-
-    Examples:
-        >>> from pyspark.sql.types import ArrayType, StringType, StructField, StructType
-        >>> schema = StructType(
-        ...     [
-        ...        StructField("studyLocusId", StringType(), False),
-        ...        StructField("locus", ArrayType(StructType([StructField("variantId", StringType(), False)])), False)
-        ...    ]
-        ... )
-        >>> df = spark.createDataFrame([("A", [{"variantId": "varA"}]), ("B", [{"variantId": "varB"}])], schema)
-        >>> flatten_schema(df.schema)
-        [Field(name='studyLocusId', dataType=StringType()), Field(name='locus', dataType=ArrayType(StructType([]), True)), Field(name='locus.variantId', dataType=StringType())]
-    """
-    Field = namedtuple("Field", ["name", "dataType"])
-    fields = []
-    for field in schema.fields:
-        name = f"{prefix}.{field.name}" if prefix else field.name
-        dtype = field.dataType
-        if isinstance(dtype, StructType):
-            fields.append(Field(name, ArrayType(StructType())))
-            fields += flatten_schema(dtype, prefix=name)
-        elif isinstance(dtype, ArrayType) and isinstance(dtype.elementType, StructType):
-            fields.append(Field(name, ArrayType(StructType())))
-            fields += flatten_schema(dtype.elementType, prefix=name)
-        else:
-            fields.append(Field(name, dtype))
-    return fields
-
-
 def compare_array_schemas(
     observed_schema: ArrayType,
     expected_schema: ArrayType,
