c286c3bb14224dd05ff21da7fec27ac07ffe1657
Irene López Santiago <45119610+ireneisdoomed@users.noreply.github.com>
2024-10-03 14:44:36 +0100

    refactor(vep_parser): store consequence to impact score as a project config (#811)
    * refactor: set variant functional consequence to pathogenicity score as class attribute
    
    * chore: drop `VariantIndex.get_most_severe_gene_consequence`
    
    * chore(VariantIndex): make `CONSEQUENCE_TO_PATHOGENICITY_SCORE` a class attribute
    
    * fix(vep): convert `id_to_score_map` to `label_to_score_map`
    
    * chore: remove comment
    
    * refactor: move `consequence_to_pathogenicity_score` to `VariantIndexConfig`

--- src/gentropy/common/spark_helpers.py
+++ src/gentropy/common/spark_helpers.py
@@ -447,14 +447,14 @@ def order_array_of_structs_by_two_fields(
     )
 
 
-def map_column_by_dictionary(col: Column, mapping_dict: dict[str, str]) -> Column:
+def map_column_by_dictionary(col: Column, mapping_dict: dict[str, Any]) -> Column:
     """Map column values to dictionary values by key.
 
     Missing consequence label will be converted to None, unmapped consequences will be mapped as None.
 
     Args:
         col (Column): Column containing labels to map.
-        mapping_dict (dict[str, str]): Dictionary with mapping key/value pairs.
+        mapping_dict (dict[str, Any]): Dictionary with mapping key/value pairs.
 
     Returns:
         Column: Column with mapped values.
