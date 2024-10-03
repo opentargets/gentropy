9e5c911f1c5802c5f4927fbb189d8d46e308cc94
Tobi Alegbe <alegbe@ebi.ac.uk>
2024-10-03 01:31:19 +0100

    fix(biosample index): revert to dropping

--- src/gentropy/dataset/study_index.py
+++ src/gentropy/dataset/study_index.py
@@ -438,7 +438,7 @@ class StudyIndex(Dataset):
                     StudyQualityCheck.UNKNOWN_BIOSAMPLE,
                 ),
             )
-            .drop("isIdFound")
+            .drop("isIdFound").drop("biosampleId")
         )
 
         return StudyIndex(_df=validated_df, _schema=StudyIndex.get_schema())
