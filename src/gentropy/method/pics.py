ca96aab17730af9d7cfb04ac936a3fe6b24f1355
David Ochoa <dogcaesar@gmail.com>
2024-10-04 11:51:18 +0100

    perf: repartition study locus before PICS to gain parallellisation

--- src/gentropy/method/pics.py
+++ src/gentropy/method/pics.py
@@ -227,7 +227,7 @@ class PICS:
         )
         return StudyLocus(
             _df=(
-                associations.df
+                associations.df.repartition()
                 # Old locus column will be dropped if available
                 .select(*[col for col in associations.df.columns if col != "locus"])
                 # Estimate neglog_pvalue for the lead variant
