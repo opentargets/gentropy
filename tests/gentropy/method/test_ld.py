75f4096705ed1f7a4ab24c603838d11100090d0a
vivienho <56025826+vivienho@users.noreply.github.com>
2024-10-07 13:44:18 +0100

    test: add tests for both of the added functions

--- tests/gentropy/method/test_ld.py
+++ tests/gentropy/method/test_ld.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pyspark.sql.functions as f
 import pyspark.sql.types as t
@@ -21,6 +21,179 @@ if TYPE_CHECKING:
 
 class TestLDAnnotator:
     """Test LDAnnotatorGnomad."""
+
+    @pytest.mark.parametrize(
+        ("observed", "expected"),
+        [
+            # no tie in relativeSampleSize
+            (
+                # observed ldPopulationStructure
+                [
+                    Row(
+                        ldPopulationStructure=[
+                            {"ldPopulation": "pop1", "relativeSampleSize": 0.5},
+                            {"ldPopulation": "pop2", "relativeSampleSize": 0.3},
+                            {"ldPopulation": "pop3", "relativeSampleSize": 0.2},
+                        ],
+                    )
+                ],
+                # expected majorPopulation
+                "pop1",
+            ),
+            # tie in relativeSampleSize, "nfe" is not one of the tied populations
+            (
+                # observed ldPopulationStructure
+                [
+                    Row(
+                        ldPopulationStructure=[
+                            {"ldPopulation": "pop1", "relativeSampleSize": 0.4},
+                            {"ldPopulation": "pop2", "relativeSampleSize": 0.4},
+                            {"ldPopulation": "pop3", "relativeSampleSize": 0.2},
+                        ],
+                    )
+                ],
+                # expected majorPopulation
+                "pop1",
+            ),
+            # tie in relativeSampleSize, "nfe" is one of the tied populations
+            (
+                # observed ldPopulationStructure
+                [
+                    Row(
+                        ldPopulationStructure=[
+                            {"ldPopulation": "pop1", "relativeSampleSize": 0.4},
+                            {"ldPopulation": "nfe", "relativeSampleSize": 0.4},
+                            {"ldPopulation": "pop3", "relativeSampleSize": 0.2},
+                        ],
+                    )
+                ],
+                # expected majorPopulation
+                "nfe",
+            ),
+        ],
+    )
+    def test__get_major_population(
+        self: TestLDAnnotator,
+        spark: SparkSession,
+        observed: list[Any],
+        expected: list[Any],
+    ) -> None:
+        """Test _get_major_population."""
+        schema = t.StructType(
+            [
+                t.StructField(
+                    "ldPopulationStructure",
+                    t.ArrayType(
+                        t.StructType(
+                            [
+                                t.StructField("ldPopulation", t.StringType(), True),
+                                t.StructField(
+                                    "relativeSampleSize", t.DoubleType(), True
+                                ),
+                            ]
+                        )
+                    ),
+                    True,
+                ),
+            ]
+        )
+        observed_df = spark.createDataFrame(observed, schema)
+        result_df = observed_df.withColumn(
+            "majorPopulation",
+            LDAnnotator._get_major_population(f.col("ldPopulationStructure")),
+        )
+        assert result_df.collect()[0]["majorPopulation"] == pytest.approx(expected)
+
+    @pytest.mark.parametrize(
+        ("observed", "expected"),
+        [
+            # r available for majorPopulation
+            (
+                # observed ldSet and majorPopulation
+                [
+                    Row(
+                        majorPopulation="pop1",
+                        ldSet=[
+                            {
+                                "tagVariantId": "tag1",
+                                "rValues": [
+                                    {"population": "pop1", "r": 0.5},
+                                    {"population": "pop2", "r": 0.6},
+                                ],
+                            }
+                        ],
+                    )
+                ],
+                # expected r2Major
+                0.25,
+            ),
+            # r not available for majorPopulation
+            (
+                # observed ldSet and majorPopulation
+                [
+                    Row(
+                        majorPopulation="pop3",
+                        ldSet=[
+                            {
+                                "tagVariantId": "tag1",
+                                "rValues": [
+                                    {"population": "pop1", "r": 0.5},
+                                    {"population": "pop2", "r": 0.6},
+                                ],
+                            }
+                        ],
+                    )
+                ],
+                # expected r2Major
+                0.0,
+            ),
+        ],
+    )
+    def test__calculate_r2_major(
+        self: TestLDAnnotator,
+        spark: SparkSession,
+        observed: list[Any],
+        expected: list[Any],
+    ) -> None:
+        """Test _calculate_r2_major."""
+        schema = t.StructType(
+            [
+                t.StructField("majorPopulation", t.StringType(), True),
+                t.StructField(
+                    "ldSet",
+                    t.ArrayType(
+                        t.StructType(
+                            [
+                                t.StructField("tagVariantId", t.StringType(), True),
+                                t.StructField(
+                                    "rValues",
+                                    t.ArrayType(
+                                        t.StructType(
+                                            [
+                                                t.StructField(
+                                                    "population", t.StringType(), True
+                                                ),
+                                                t.StructField(
+                                                    "r", t.DoubleType(), True
+                                                ),
+                                            ]
+                                        )
+                                    ),
+                                    True,
+                                ),
+                            ]
+                        )
+                    ),
+                    True,
+                ),
+            ]
+        )
+        observed_df = spark.createDataFrame(observed, schema)
+        result_df = observed_df.withColumn(
+            "ldSet",
+            LDAnnotator._calculate_r2_major(f.col("ldSet"), f.col("majorPopulation")),
+        )
+        assert result_df.collect()[0]["ldSet"][0]["r2Major"] == pytest.approx(expected)
 
     def test__add_population_size(
         self: TestLDAnnotator,
@@ -48,9 +221,7 @@ class TestLDAnnotator:
             ),
         ).withColumn("ldSet", LDAnnotator._calculate_weighted_r_overall(f.col("ldSet")))
         expected = 0.2
-        assert result_df.collect()[0]["ldSet"][0]["r2Overall"] == pytest.approx(
-            expected
-        )
+        assert result_df.collect()[0]["ldSet"][0]["r2Major"] == pytest.approx(expected)
 
     def test__rescue_lead_variant(self: TestLDAnnotator, spark: SparkSession) -> None:
         """Test _rescue_lead_variant."""
@@ -65,7 +236,7 @@ class TestLDAnnotator:
                             t.StructType(
                                 [
                                     t.StructField("tagVariantId", t.StringType(), True),
-                                    t.StructField("r2Overall", t.DoubleType(), True),
+                                    t.StructField("r2Major", t.DoubleType(), True),
                                 ]
                             )
                         ),
@@ -79,7 +250,7 @@ class TestLDAnnotator:
             LDAnnotator._rescue_lead_variant(f.col("ldSet"), f.col("variantId")),
         )
         assert (
-            result_df.select("ldSet.r2Overall").collect()[0]["r2Overall"][0] == 1.0
+            result_df.select("ldSet.r2Major").collect()[0]["r2Major"][0] == 1.0
         ), "ldSet does not contain the rescued lead variant."
 
     @pytest.fixture(autouse=True)
