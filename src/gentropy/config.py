4261770220f9e6b4a5fbb3f0fbd637aea0413590
Irene López <irene.lopezs@protonmail.com>
2024-10-15 15:14:38 +0100

    fix(l2g): set `feature_matrix_path` as mandatory in step config

--- src/gentropy/config.py
+++ src/gentropy/config.py
@@ -225,9 +225,9 @@ class LocusToGeneConfig(StepConfig):
     run_mode: str = MISSING
     predictions_path: str = MISSING
     credible_set_path: str = MISSING
+    feature_matrix_path: str = MISSING
     variant_index_path: str | None = None
     model_path: str | None = None
-    feature_matrix_path: str | None = None
     gold_standard_curation_path: str | None = None
     gene_interactions_path: str | None = None
     features_list: list[str] = field(
