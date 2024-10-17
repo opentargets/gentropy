aec355ddc5c344e5a7684910a641535c8062fad3
Tobi Alegbe <alegbe@ebi.ac.uk>
2024-10-17 13:07:41 +0100

    feat(biosample_index_add_efo): add biosample index efo tests

--- tests/gentropy/datasource/biosample_ontologies/test_biosample_ontology.py
+++ tests/gentropy/datasource/biosample_ontologies/test_biosample_ontology.py
@@ -16,6 +16,7 @@ class TestOntologyParger:
 
     SAMPLE_CELL_ONTOLOGY_PATH = "tests/gentropy/data_samples/cell_ontology_sample.json"
     SAMPLE_UBERON_PATH = "tests/gentropy/data_samples/uberon_sample.json"
+    SAMPLE_EFO_PATH = "tests/gentropy/data_samples/efo_biosample_sample.json"
 
     def test_cell_ontology_parser(
         self: TestOntologyParger, spark: SparkSession
@@ -35,6 +36,26 @@ class TestOntologyParger:
             uberon, BiosampleIndex
         ), "Uberon subset is not parsed correctly to BiosampleIndex."
 
+    def test_ontology_parser(self: TestOntologyParger, spark: SparkSession) -> None:
+        """Test all ontology parsers."""
+        cell_ontology = extract_ontology_from_json(
+            self.SAMPLE_CELL_ONTOLOGY_PATH, spark
+        )
+        uberon = extract_ontology_from_json(self.SAMPLE_UBERON_PATH, spark)
+        efo_cell_line = extract_ontology_from_json(
+            self.SAMPLE_EFO_PATH, spark
+        ).retain_rows_with_ancestor_id(["CL_0000000"])
+
+        assert isinstance(
+            cell_ontology, BiosampleIndex
+        ), "Cell ontology subset is not parsed correctly to BiosampleIndex."
+        assert isinstance(
+            uberon, BiosampleIndex
+        ), "Uberon subset is not parsed correctly to BiosampleIndex."
+        assert isinstance(
+            efo_cell_line, BiosampleIndex
+        ), "EFO cell line subset is not parsed correctly to BiosampleIndex."
+
     def test_merge_biosample_indices(
         self: TestOntologyParger, spark: SparkSession
     ) -> None:
@@ -43,8 +64,9 @@ class TestOntologyParger:
             self.SAMPLE_CELL_ONTOLOGY_PATH, spark
         )
         uberon = extract_ontology_from_json(self.SAMPLE_UBERON_PATH, spark)
+        efo = extract_ontology_from_json(self.SAMPLE_EFO_PATH, spark)
 
-        merged = cell_ontology.merge_indices([uberon])
+        merged = cell_ontology.merge_indices([uberon, efo])
         assert isinstance(
             merged, BiosampleIndex
         ), "Merging of biosample indices is not correct."
