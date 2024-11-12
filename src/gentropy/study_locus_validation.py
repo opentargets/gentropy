506d4c38c52d5ed67a3c04dbcc78a7489de14ad1
Daniel Considine <dc16@sanger.ac.uk>
2024-11-12 21:22:54 +0000

    fix: study_locus_validation

--- src/gentropy/study_locus_validation.py
+++ src/gentropy/study_locus_validation.py
@@ -58,12 +58,12 @@ class StudyLocusValidationStep:
 
         # Valid study locus partitioned to simplify the finding of overlaps
         study_locus_with_qc.valid_rows(
-            invalid_qc_reasons, invalid=True
+            invalid_qc_reasons,
         ).df.repartitionByRange("chromosome", "position").sortWithinPartitions(
             "chromosome", "position"
-        ).write.mode(session.write_mode).parquet(invalid_study_locus_path)
+        ).write.mode(session.write_mode).parquet(valid_study_locus_path)
 
-        # Infalid study locus
-        study_locus_with_qc.valid_rows(invalid_qc_reasons).df.write.mode(
+        # Invalid study locus
+        study_locus_with_qc.valid_rows(invalid_qc_reasons, invalid=True).df.write.mode(
             session.write_mode
-        ).parquet(valid_study_locus_path)
+        ).parquet(invalid_study_locus_path)
