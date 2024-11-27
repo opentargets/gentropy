4837a4b4add387ac9e1edb3fba7fbbbea712bc80
Irene López Santiago <45119610+ireneisdoomed@users.noreply.github.com>
2024-11-27 10:07:42 +0000

    feat(gold_standard): add traitFromSourceMappedId to schema (#924)
    * feat(gold_standard): add traitFromSourceMappedId to schema
    
    * chore: adapt tests
    
    * feat(feature_matrix): consider `traitFromSourceMappedId` a static column
    
    * feat(feature_matrix): consider `traitFromSourceMappedId` an optional column

--- src/gentropy/dataset/l2g_feature_matrix.py
+++ src/gentropy/dataset/l2g_feature_matrix.py
@@ -39,6 +39,8 @@ class L2GFeatureMatrix:
         self.fixed_cols = ["studyLocusId", "geneId"]
         if self.with_gold_standard:
             self.fixed_cols.append("goldStandardSet")
+        if "traitFromSourceMappedId" in _df.columns:
+            self.fixed_cols.append("traitFromSourceMappedId")
 
         self.features_list = features_list or [
             col for col in _df.columns if col not in self.fixed_cols
