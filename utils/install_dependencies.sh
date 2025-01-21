7f416eded4bea7c029e4ca65ecbe53c947250d4c
Szymon Szyszkowski <ss60@mib117351s.internal.sanger.ac.uk>
2025-01-21 14:59:27 +0000

    chore: fix python version in setup dev script

--- utils/install_dependencies.sh
+++ utils/install_dependencies.sh
@@ -1,23 +1,20 @@
+readonly PYTHON_VERSION=3.10.8
+export SHELL_RC=$(echo "$HOME/.${SHELL##*/}rc")
+
 if ! command -v pyenv &>/dev/null; then
     echo "Installing Pyenv, a tool to manage multiple Python versions..."
     curl -sSL https://pyenv.run | bash
-    # Add Pyenv configuration to ~/.bashrc.
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    # Add Pyenv configuration to rc file.
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >>$SHELL_RC
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>$SHELL_RC
+    echo 'eval "$(pyenv init -)"' >>$SHELL_RC
     # And also execute it right now.
-    . <(tail -n3 ~/.bashrc)
+    . <(tail -n3 $SHELL_RC)
 fi
 
 echo "Activating Pyenv environment with a Python version required for the project..."
-if [ -f ".python-version" ]; then
-    PYTHON_VERSION=$(cat .python-version)
-    pyenv install --skip-existing $PYTHON_VERSION
-    pyenv shell $PYTHON_VERSION
-else
-    echo ".python-version file not found."
-    exit 1
-fi
+pyenv install --skip-existing $PYTHON_VERSION
+pyenv shell $PYTHON_VERSION
 
 if ! command -v poetry &>/dev/null; then
     echo "Installing Poetry, a tool to manage Python dependencies..."
