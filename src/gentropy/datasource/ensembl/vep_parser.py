4eabc511504b99717257d540f074e8b44a8b6ca9
Szymon Szyszkowski <ss60@mib117351s.internal.sanger.ac.uk>
2025-01-21 17:07:16 +0000

    Revert "Revert "chore: fix doctest syntax""
    This reverts commit 18c66b1d1d9c4833e3dbfc406ba3440577a119a5.

--- src/gentropy/datasource/ensembl/vep_parser.py
+++ src/gentropy/datasource/ensembl/vep_parser.py
@@ -192,7 +192,7 @@ class VariantEffectPredictorParser:
             |rsIds          |
             +---------------+
             |[rs1, rs2, rs3]|
-            |[null]         |
+            |[NULL]         |
             +---------------+
             <BLANKLINE>
         """
@@ -380,8 +380,8 @@ class VariantEffectPredictorParser:
         +-----------------------------------------------------+
         |am                                                   |
         +-----------------------------------------------------+
-        |{AlphaMissense, assessment 1, 0.4, null, gene1, null}|
-        |{AlphaMissense, null, null, null, gene1, null}       |
+        |{AlphaMissense, assessment 1, 0.4, NULL, gene1, NULL}|
+        |{AlphaMissense, NULL, NULL, NULL, gene1, NULL}       |
         +-----------------------------------------------------+
         <BLANKLINE>
         """
@@ -513,8 +513,8 @@ class VariantEffectPredictorParser:
             |amino_acid_change|
             +-----------------+
             |              A1B|
-            |             null|
-            |             null|
+            |             NULL|
+            |             NULL|
             +-----------------+
             <BLANKLINE>
         """
