89a9c34168e95de0715239b2ec116cdf998f19ae
Szymon Szyszkowski <ss60@mib117351s.internal.sanger.ac.uk>
2025-01-23 11:50:14 +0000

    build: new setup

--- utils/clean_status.sh
+++ utils/clean_status.sh
@@ -4,7 +4,7 @@ echo "Fetching version changes..."
 git fetch
 if output=$(git status --porcelain) && [ -z "$output" ]; then
     echo "Checking if branch is up-to-date with remote..."
-elif output=$(git push --dry-run --porcelain) && [[ "$output" == "*[up to date]*" ]]; then
+elif output=$(git push --dry-run --porcelain) && [[ "$output" == *"[up to date]"* ]]; then
     exit 0
 else
     exit 1
