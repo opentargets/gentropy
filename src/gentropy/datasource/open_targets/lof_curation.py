2ff0455200941c73f3485ad13fecc78470a55bf8
vivienho <56025826+vivienho@users.noreply.github.com>
2025-02-07 14:01:21 +0000

    feat: add function for creating LOF description

--- src/gentropy/datasource/open_targets/lof_curation.py
+++ src/gentropy/datasource/open_targets/lof_curation.py
@@ -37,6 +37,26 @@ class OpenTargetsLOF:
             verdict.alias("assessment"),
         )
 
+    @staticmethod
+    def _compose_lof_description(verdict: Column) -> Column:
+        """Compose variant description based on loss-of-function assessment.
+
+        Args:
+            verdict (Column): verdict column from the input dataset.
+
+        Returns:
+            Column: variant description.
+        """
+        lof_description = (
+            f.when(verdict == "lof", f.lit("Assessed to cause LoF"))
+            .when(verdict == "likely_lof", f.lit("Suspected to cause LoF"))
+            .when(verdict == "uncertain", f.lit("Uncertain LoF assessment"))
+            .when(verdict == "likely_not_lof", f.lit("Suspected not to cause LoF"))
+            .when(verdict == "not_lof", f.lit("Assessed not to cause LoF"))
+        )
+
+        return f.concat(lof_description, f.lit(" by OTAR2075 variant curation effort."))
+
     @classmethod
     def as_variant_index(
         cls: type[OpenTargetsLOF],
@@ -69,6 +89,7 @@ class OpenTargetsLOF:
                     f.col("h37.alt").alias("alternateAllele"),
                     # Populate inSilicoPredictors field:
                     f.array(cls._get_lof_assessment(f.col("Verdict"))).alias("inSilicoPredictors"),
+                    cls._compose_lof_description(f.col("Verdict")).alias("variantDescription"),
                 )
                 # Convert assessments to normalised scores:
                 .withColumn("inSilicoPredictors", InSilicoPredictorNormaliser.normalise_in_silico_predictors(f.col("inSilicoPredictors")))
