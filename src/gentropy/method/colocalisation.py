7513f4b4cffe74214471d5a21518f4ca1dc37e5b
Daniel Considine <dc16@sanger.ac.uk>
2025-03-26 10:20:22 +0000

    Revert "feat: adjusting the NA logBF value"
    This reverts commit 863738b3376290ea987221765c17684d65e7df1d.

--- src/gentropy/method/colocalisation.py
+++ src/gentropy/method/colocalisation.py
@@ -289,15 +289,10 @@ class Coloc(ColocalisationMethodInterface):
                 .fillna(
                     0,
                     subset=[
-                        "left_posteriorProbability",
-                        "right_posteriorProbability",
-                    ],
-                )
-                .fillna(
-                    -6.907,
-                    subset=[
                         "left_logBF",
                         "right_logBF",
+                        "left_posteriorProbability",
+                        "right_posteriorProbability",
                     ],
                 )
                 # Sum of log_BFs for each pair of signals
