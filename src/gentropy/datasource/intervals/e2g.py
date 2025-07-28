6d08a865e5dcf6a07211b252b3a6c8c40f720f8e
xyg123 <xiangyuge123@hotmail.co.uk>
2025-07-28 14:41:55 +0100

    feat: logic to ingest e2g

--- src/gentropy/datasource/intervals/e2g.py
+++ src/gentropy/datasource/intervals/e2g.py
@@ -13,37 +13,37 @@ if TYPE_CHECKING:
     from pyspark.sql import DataFrame, SparkSession
 
 
-class IntervalsEpiraction:
-    """Interval dataset from EPIraction."""
+class IntervalsE2G:
+    """Interval dataset from E2G."""
 
     @staticmethod
     def read(spark: SparkSession, path: str) -> DataFrame:
-        """Read EPIraction dataset.
+        """Read E2G dataset.
 
         Args:
             spark (SparkSession): Spark session
             path (str): Path to the dataset
 
         Returns:
-            DataFrame: Raw, full EPIraction dataframe
+            DataFrame: Raw, full E2G dataframe
         """
         return (
             spark.read.option("delimiter", "\t")
-            .option("mode", "DROPMALFORMED")
             .option("header", "true")
             .csv(path)
+            .withColumn("file_path", f.input_file_name())
         )
 
     @classmethod
     def parse(
-        cls: type[IntervalsEpiraction],
-        raw_epiraction_df: DataFrame,
+        cls: type[IntervalsE2G],
+        raw_e2g_df: DataFrame,
         target_index: TargetIndex,
     ) -> Intervals:
-        """Parse EPIraction dataset.
+        """Parse E2G dataset.
 
         Args:
-            raw_epiraction_df (DataFrame): Raw EPIraction dataset
+            raw_e2g_df (DataFrame): Raw E2G dataset
             target_index (TargetIndex): Target index
 
         Returns:
@@ -53,71 +53,58 @@ class IntervalsEpiraction:
         dataset_name = "epiraction"
         pmid = "40027634"  # PMID for the EPIraction paper
 
-        # Read the epiraction file:
-        parsed_epiraction_df = (
-            raw_epiraction_df.filter(
-                (f.col("class") == "enhancer") | (f.col("class") == "promoter")
+        # Read the e2g file:
+        parsed_e2g_df = (
+            raw_e2g_df.withColumn(
+                "studyId", f.regexp_extract(f.col("file_path"), r"([^/]+)\.bed\.gz$", 1)
             )
-            .withColumn(
-                "chromosome",
-                f.regexp_replace(f.col("chr"), "^chr", ""),
-            )
+            .withColumn("chromosome", f.regexp_replace("#chr", "^chr", ""))
             .withColumnRenamed("TargetGeneEnsemblID", "geneId")
             .withColumnRenamed("CellType", "biosampleName")
             .withColumnRenamed("Score", "score")
             .withColumnRenamed("class", "intervalType")
+            .withColumnRenamed("TargetGeneTSS", "distanceToTSS")
             .withColumn(
                 "resourceScore",
                 f.array(
                     f.struct(
-                        f.lit("H3K27ac").alias("name"),
-                        f.col("H3K27ac").cast("float").alias("value"),
-                    ),
-                    f.struct(
-                        f.lit("Open").alias("name"),
-                        f.col("Open").cast("float").alias("value"),
-                    ),
-                    f.struct(
-                        f.lit("Cofactor").alias("name"),
-                        f.col("Cofactor").cast("float").alias("value"),
-                    ),
-                    f.struct(
-                        f.lit("CTCF").alias("name"),
-                        f.col("CTCF").cast("float").alias("value"),
+                        f.lit("DNase").alias("name"),
+                        f.col("`normalizedDNase_prom.Feature`")
+                        .cast("float")
+                        .alias("value"),
                     ),
                     f.struct(
                         f.lit("HiC_contacts").alias("name"),
-                        f.col("HiC_contacts").cast("float").alias("value"),
-                    ),
-                    f.struct(
-                        f.lit("abc_tissue").alias("name"),
-                        f.col("abc_tissue").cast("float").alias("value"),
+                        f.col("`3DContact.Feature`").cast("float").alias("value"),
                     ),
                 ),
             )
         )
 
-        parsed_epiraction_df = parsed_epiraction_df.join(
+        parsed_e2g_df = parsed_e2g_df.join(
             target_index._df.select(f.col("id").alias("geneId")),
             on="geneId",
             how="inner",
         )
 
+        dataset_name = "E2G"
+        pmid = "38014075"  # PMID for the EPIraction paper
+
         return Intervals(
             _df=(
-                parsed_epiraction_df
-                # Select relevant columns:
-                .select(
+                parsed_e2g_df.select(
                     f.col("chromosome"),
-                    f.col("start"),
-                    f.col("end"),
+                    f.col("start").cast("string"),
+                    f.col("end").cast("string"),
                     f.col("geneId"),
                     f.col("biosampleName"),
                     f.col("intervalType"),
-                    f.col("score"),
+                    f.col("distanceToTSS").cast("double"),
+                    f.col("score").cast("double"),
                     f.col("resourceScore"),
                     f.lit(dataset_name).alias("datasourceId"),
                     f.lit(pmid).alias("pmid"),
+                    f.col("studyId"),
                 )
             ),
             _schema=Intervals.get_schema(),
