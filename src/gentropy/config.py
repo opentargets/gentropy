0792942f2cd454e55d1c94fdecee40a53bdb71b1
Irene López Santiago <45119610+ireneisdoomed@users.noreply.github.com>
2025-08-04 14:29:42 +0100

    fix(l2g): move `append_null_features` param to feature matrix step (#1078)
    Co-authored-by: Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>

--- src/gentropy/config.py
+++ src/gentropy/config.py
@@ -287,7 +287,6 @@ class LocusToGeneConfig(StepConfig):
     download_from_hub: bool = True
     cross_validate: bool = True
     explain_predictions: bool | None = False
-    append_null_features: bool = False
     _target_: str = "gentropy.l2g.LocusToGeneStep"
 
 
@@ -350,6 +349,7 @@ class LocusToGeneFeatureMatrixConfig(StepConfig):
             "isProteinCoding",
         ]
     )
+    append_null_features: bool = False
     _target_: str = "gentropy.l2g.LocusToGeneFeatureMatrixStep"
 
 
