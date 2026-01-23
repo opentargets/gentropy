ff8020ab873f82ffc315aed93b6da76f96f866be
PROEJCT-DEFIANT <szymonszyszkowskI@gmail.com>
2026-01-23 16:19:33 +0000

    chore: tests for study index

--- tests/gentropy/step/test_decode_step.py
+++ tests/gentropy/step/test_decode_step.py
@@ -8,18 +8,21 @@ import pytest
 from pyspark.sql import DataFrame, Row
 
 from gentropy import Session
-from gentropy.decode_ingestion import deCODEManifestGenerationStep
+from gentropy.decode_ingestion import (
+    deCODEManifestGenerationStep,
+    deCODEStudyIndexGenerationStep,
+)
 
 
 @pytest.fixture
-def manifest_df(session: Session) -> DataFrame:
+def decode_manifest_df(session: Session) -> DataFrame:
     """Manifest example DataFrame fixture."""
     data = [
         Row(
             projectId="deCODE-proteomics-raw",
             studyId="deCODE-proteomics-raw_Proteomics_PC0_10006_7_RAF1_c_Raf_05314415",
             hasSumstats=True,
-            summarystatsLocation="s3a://some_bucket/final_somascan_raw/Proteomics_PC0_10006_7_RAF1_c_Raf_05314415.txt.gz",
+            summarystatsLocation="s3a://some_bucket/some_folder/Proteomics_PC0_10006_7_RAF1_c_Raf_05314415.txt.gz",
             size="925.8 MiB",
             accessionTimestamp=datetime.datetime(2024, 5, 29, 7, 39, 17),
         ),
@@ -27,7 +30,7 @@ def manifest_df(session: Session) -> DataFrame:
             projectId="deCODE-proteomics-raw",
             studyId="deCODE-proteomics-raw_Proteomics_PC0_10003_15_ZNF41_ZNF41_05242112",
             hasSumstats=True,
-            summarystatsLocation="s3a://some_bucket/final_somascan_raw/Proteomics_PC0_10003_15_ZNF41_ZNF41_05242112.txt.gz",
+            summarystatsLocation="s3a://some_bucket/some_folder/Proteomics_PC0_10003_15_ZNF41_ZNF41_05242112.txt.gz",
             size="926.9 MiB",
             accessionTimestamp=datetime.datetime(2024, 5, 29, 7, 39, 21),
         ),
@@ -35,10 +38,57 @@ def manifest_df(session: Session) -> DataFrame:
     return session.spark.createDataFrame(data)
 
 
-class TestdeCODEManifestGenerationStep:
+@pytest.fixture()
+def decode_study_index_df(session: Session) -> DataFrame:
+    """StudyIndex example mock."""
+    data = [
+        Row(
+            studyId="deCODE-proteomics-raw_Proteomics_PC0_10006_7_RAF1_c_Raf_05314415",
+            projectId="deCODE-proteomics-raw",
+            studyType="pqtl",
+            traitFromSource="Proteomics_PC0_10006_7_RAF1_c_Raf",
+            geneId="ENSG00000132155",
+            biosampleFromSourceId="UBERON_0001969",
+            pubmedId="37794188",
+            publicationTitle="Large-scale plasma proteomics comparisons through genetics and disease associations",
+            publicationFirstAuthor="Eldjarn GH, Ferkingstad E",
+            publicationDate="2024",
+            publicationJournal="Nature",
+            initialSampleSize="36,136 Icelandic individuals",
+            nSamples=36136,
+            cohorts=["deCODE"],
+            ldPopulationStructure=[Row(ldPopulation="nfe", relativeSampleSize=1.0)],
+            discoverySamples=[Row(sampleSize=36136, ancestry="Icelandic")],
+            summarystatsLocation="s3a://some_bucket/some_folder/Proteomics_PC0_10001_7_RAF1_c_Raf_07082019.txt.gz",
+            hasSumstats=True,
+        ),
+        Row(
+            studyId="deCODE-proteomics-raw_Proteomics_PC0_10003_15_ZNF41_ZNF41_05242112",
+            projectId="deCODE-proteomics-raw",
+            studyType="pqtl",
+            traitFromSource="Proteomics_PC0_10003_15_ZNF41_ZNF41",
+            geneId="ENSG00000147124",
+            biosampleFromSourceId="UBERON_0001969",
+            pubmedId="37794188",
+            publicationTitle="Large-scale plasma proteomics comparisons through genetics and disease associations",
+            publicationFirstAuthor="Eldjarn GH, Ferkingstad E",
+            publicationDate="2024",
+            publicationJournal="Nature",
+            initialSampleSize="36,136 Icelandic individuals",
+            nSamples=36136,
+            cohorts=["deCODE"],
+            ldPopulationStructure=[Row(ldPopulation="nfe", relativeSampleSize=1.0)],
+            discoverySamples=[Row(sampleSize=36136, ancestry="Icelandic")],
+            summarystatsLocation="s3a://some_bucket/some_folder/Proteomics_PC0_10003_15_ZNF41_ZNF41_07082019.txt.gz",
+            hasSumstats=True,
+        ),
+    ]
+    return session.spark.createDataFrame(data)
+
+
+class TestdeCODEIngestionStep:
     """Test deCODEManifestGenerationStep."""
 
-    # @pytest.mark.skip(reason="Not implemented yet")
     @patch("gentropy.decode_ingestion.S3Config")
     @patch("gentropy.decode_ingestion.deCODEManifest")
     def test_decode_manifest_generation_step(
@@ -47,7 +97,7 @@ class TestdeCODEManifestGenerationStep:
         s3_config_mock: MagicMock,
         session: Session,
         tmp_path: Path,
-        manifest_df: DataFrame,
+        decode_manifest_df: DataFrame,
     ):
         """Test deCODEManifestGenerationStep."""
         s3_config_path = (tmp_path / "s3_config.json").as_posix()
@@ -56,9 +106,8 @@ class TestdeCODEManifestGenerationStep:
 
         # Mock the instance of the manifest
         manifest_instance = MagicMock()
-        manifest_instance.df = manifest_df
+        manifest_instance.df = decode_manifest_df
 
-        manifest_df.show()
         # Link the instance to return_value from the constructor
         manifest_mock.from_bucket_listing.return_value = manifest_instance
 
@@ -78,14 +127,52 @@ class TestdeCODEManifestGenerationStep:
         )
         assert Path(output_path).exists()
 
+    @pytest.mark.skip(reason="Not implemented yet")
+    @patch("gentropy.decode_ingestion.deCODEManifest")
+    @patch("gentropy.decode_ingestion.TargetIndex")
+    @patch("gentropy.decode_ingestion.deCODEStudyIndex")
+    def test_decode_study_index_generation_step(
+        self,
+        study_index_mock: MagicMock,
+        target_index_mock: MagicMock,
+        manifest_mock: MagicMock,
+        session: Session,
+        tmp_path: Path,
+        decode_study_index_df: DataFrame,
+    ):
+        """Test deCODEStudyIndexGenerationStep."""
+        manifest_path = (tmp_path / "manifest.parquet").as_posix()
+        target_index_path = (tmp_path / "target_index.parquet").as_posix()
+        output_path = (tmp_path / "study_index_output").as_posix()
 
-# class TestdeCODEStudyIndexGenerationStep:
-#     """Test deCODEStudyIndexGenerationStep."""
+        # Mock the instances
+        manifest_instance = MagicMock()
+        target_index_instance = MagicMock()
+        study_index_instance = MagicMock()
+        study_index_instance.df = decode_study_index_df
 
-#     @pytest.mark.skip(reason="Not implemented yet")
-#     def test_decode_study_index_generation_step(self):
-#         """Test deCODEStudyIndexGenerationStep."""
-#         pass
+        # Link the instances to return_value from the constructors
+        manifest_mock.from_path.return_value = manifest_instance
+        target_index_mock.from_parquet.return_value = target_index_instance
+        study_index_mock.from_manifest.return_value = study_index_instance
+
+        deCODEStudyIndexGenerationStep(
+            session=session,
+            manifest_path=manifest_path,
+            target_index_path=target_index_path,
+            output_path=output_path,
+        )
+
+        manifest_mock.from_path.assert_called_once_with(
+            session=session, path=manifest_path
+        )
+        target_index_mock.from_parquet.assert_called_once_with(
+            session=session, path=target_index_path
+        )
+        study_index_mock.from_manifest.assert_called_once_with(
+            manifest=manifest_instance, target_index=target_index_instance
+        )
+        assert Path(output_path).exists()
 
 
 # class TestdeCODESummaryStatisticsIngestionStep:
